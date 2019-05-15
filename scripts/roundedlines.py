e31b5e8e1d22239a1a37c19bbc11970cd658ea2f
Axel Ringhausen <ringhausen@dessia.tech>
2019-05-15 14:57:20 +0200

    Commit 1 : scripts

--- scripts/roundedlines.py
+++ scripts/roundedlines.py
@@ -16,15 +16,21 @@ import volmdlr.primitives3D as primitives3D
 # =============================================================================
 
 p1 = vm.Point2D((0, 0))
+p11 = vm.Point2D((0.5, 0))
 p2 = vm.Point2D((1, 0))
 p3 = vm.Point2D((1, 1))
 p4 = vm.Point2D((2.1, 3.23))
 p5 = vm.Point2D((0, 1.23))
+#p4 = vm.Point2D((0,1))
+#p5 = vm.Point2D((0,2))
 
 
-rl2D_o = primitives2D.RoundedLineSegments2D([p1, p2, p3, p4, p5],
-                                        {1:0.3, 2:0.1, 3:0.5},
+
+rl2D_o = primitives2D.RoundedLineSegments2D([p1, p11, p2, p3, p4, p5],
+                                        {2:0.3, 4:0.1, 3:0.1},
                                         closed=False, adapt_radius=True)
+
+
 
 rl2D_o2 = rl2D_o.Offset(0.1)
 f, ax= rl2D_o.MPLPlot()
@@ -32,14 +38,16 @@ rl2D_o2.MPLPlot(ax=ax, style='r')
 
 
 rl2D_c = primitives2D.RoundedLineSegments2D([p1, p2, p3, p4, p5],
-                                        {0: 0.7, 1:0.3, 2:0.1, 3:2},
+                                        {0: 1, 1:0.05, 2:0.05, 3:1},
                                         closed=True, adapt_radius=True)
-rl2D_c.MPLPlot()
+rl2D_c2 = rl2D_c.Offset(0.2)
+f2, ax2 = rl2D_c.MPLPlot()
+rl2D_c2.MPLPlot(ax=ax2, style='r')
 
 
-## =============================================================================
-##  3D Version
-## =============================================================================
+# =============================================================================
+#  3D Version
+# =============================================================================
 #
 #p1 = vm.Point3D((0, 0, 0))
 #p2 = vm.Point3D((1, 0, 0))
@@ -54,3 +62,5 @@ rl2D_c.MPLPlot()
 #rl3D.MPLPlot()
 #m = vm.VolumeModel([('rl3D', [rl3D])])
 #m.FreeCADExport('rl3D')
+
+#{1:0.3, 2:0.1, 3:0.5}
