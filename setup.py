109bdcee0e159bf0949755e9940429e3d9ed3174
Steven Masfaraud <masfaraud@dessia.tech>
2020-02-04 12:11:30 +0100

    Setup enhancement and manifest

--- setup.py
+++ setup.py
@@ -28,25 +28,38 @@ def version_from_git_describe(version):
     if '-' in version:
         version, number_commits_ahead, commit_hash = version.split('-')
         number_commits_ahead = int(number_commits_ahead)
-
+        
+    print('number_commits_ahead', number_commits_ahead)
+    
+    print('____')
     split_versions = version.split('.')
-    if len(split_versions) == 4:
-        suffix = split_versions[3]
-        split_versions = split_versions[:3]
+    print(split_versions)
+    if 'post' in split_versions[-1]:
+        suffix = split_versions[-1]
+        split_versions = split_versions[:-1]
     else:
         suffix = None
     
-    for suffix2 in ['a', 'b', 'rc']:
-        if suffix2 in split_versions[-1]:
+    for pre_release_segment in ['a', 'b', 'rc']:
+        if pre_release_segment in split_versions[-1]:
             if number_commits_ahead > 0:
-                split_versions[-1] = str(split_versions[-1].split(suffix2)[0])
+                split_versions[-1] = str(split_versions[-1].split(pre_release_segment)[0])
+                if len(split_versions) == 2:
+                    split_versions.append('0')
+                if len(split_versions) == 1:
+                    split_versions.extend(['0', '0'])
+        
                 split_versions[-1] = str(int(split_versions[-1])+1)
                 future_version = '.'.join(split_versions)
                 return '{}.dev{}'.format(future_version, number_commits_ahead)
-
             else:
                 return '.'.join(split_versions)
+            
     if number_commits_ahead > 0:
+        if len(split_versions) == 2:
+            split_versions.append('0')
+        if len(split_versions) == 1:
+            split_versions.extend(['0', '0'])
         split_versions[-1] = str(int(split_versions[-1])+1)
         split_versions = '.'.join(split_versions)
         return '{}.dev{}'.format(split_versions, number_commits_ahead)
@@ -57,8 +70,11 @@ def version_from_git_describe(version):
         return '.'.join(split_versions)
     
 # Just testing if get_version works well
-for v in ['v0.1.7.post2', 'v0.0.1-25-gaf0bf53', 'v0.0.1a2-25-gaf0bf53']:
-    version_from_git_describe(v)
+assert version_from_git_describe('v0.1.7.post2') == '0.1.7.post2'
+assert version_from_git_describe('v0.0.1-25-gaf0bf53') == '0.0.2.dev25'
+assert version_from_git_describe('v0.1-15-zsdgaz') == '0.1.1.dev15'
+assert version_from_git_describe('v1') == '1'
+assert version_from_git_describe('v1-3-aqsfjbo') == '1.0.1.dev3'
     
 def get_version():
     # Return the version if it has been injected into the file by git-archive
@@ -92,7 +108,7 @@ setup(name='volmdlr',
 #      setup_requires=['setuptools_scm'],
       description=' A volume modeler computation-oriented. Include rendering bindings. ',
       long_description=readme(),
-      keywords='volume, modeler',
+      keywords='volume, modeler, CAD',
       url='https://github.com/Dessia-tech/volmdlr',
       author='DessiA Technologies',
       author_email='root@dessia.tech',
