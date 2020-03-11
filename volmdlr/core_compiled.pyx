9bb49b8681e058f16d156ad19ec27731b778d40d
Steven Masfaraud <masfaraud@dessia.tech>
2020-03-11 09:58:08 +0100

    Adding typings and pyproject in manifest

--- volmdlr/core_compiled.pyx
+++ volmdlr/core_compiled.pyx
@@ -636,7 +636,7 @@ class Vector3D(Vector):
     #         }
     #     }
 
-    def __init__(self, vector::Tuple[float, float, float], name:str=''):
+    def __init__(self, vector:Tuple[float, float, float], name:str=''):
         self.vector = [0, 0, 0]
 #        self.vector = npy.zeros(3)
         self.vector[0] = vector[0]
@@ -663,7 +663,7 @@ class Vector3D(Vector):
                          self.vector[1] / value,
                          self.vector[2] / value))
 
-    def __round__(self, ndigits=6):
+    def __round__(self, ndigits:int=6):
         return self.__class__((round(self.vector[0], ndigits),
                                round(self.vector[1], ndigits),
                                round(self.vector[2], ndigits)))
@@ -671,7 +671,7 @@ class Vector3D(Vector):
     def __hash__(self):
         return int(1000*(self.vector[0]+self.vector[1]+self.vector[2]))
 
-    def __eq__(self, other_vector):
+    def __eq__(self, other_vector:'Vector3D'):
         if self.__class__ != other_vector.__class__:
             return False
         return math.isclose(self.vector[0], other_vector.vector[0], abs_tol=1e-08) \
@@ -683,15 +683,15 @@ class Vector3D(Vector):
         ov1, ov2, ov3 = other_vector.vector
         return CVector3DDot(v1, v2, v3, ov1, ov2, ov3)
 
-    def Cross(self, other_vector):
+    def Cross(self, other_vector:'Vector3D') -> 'Vector3D':
         return self.__class__(vector3D_cross(self.vector, other_vector.vector))
 
-    def Norm(self):
+    def Norm(self) -> float:
         vx, vy, vz = self.vector
         return CVector3DNorm(vx, vy, vz)
 
 
-    def Normalize(self):
+    def Normalize(self) -> 'Vector3D':
         """
         Normalize the vector modifying it's coordinate
         """
@@ -703,10 +703,10 @@ class Vector3D(Vector):
         self.vector[1] /= n
         self.vector[2] /= n
 
-    def point_distance(self, point2):
+    def point_distance(self, point2:'Vector3D') -> float:
         return (self-point2).Norm()
 
-    def Rotation(self, center, axis, angle, copy=True):
+    def Rotation(self, center:'Point3D', axis:'Vector3D', angle:float, copy:bool=True):
         """
         Rotation of angle around axis.
         Used Rodrigues Formula:
@@ -724,7 +724,7 @@ class Vector3D(Vector):
         else:
             self.vector = list(vector2)
 
-    def x_rotation(self, angle, copy=True):
+    def x_rotation(self, angle:float, copy:bool=True):
         """
         Rotation of angle around X axis.
         """
@@ -741,7 +741,7 @@ class Vector3D(Vector):
             self.vector[1] = y1
             self.vector[2] = z1
 
-    def y_rotation(self, angle, copy=True):
+    def y_rotation(self, angle:float, copy:bool=True):
         """
         Rotation of angle around Y axis.
         """
@@ -758,7 +758,7 @@ class Vector3D(Vector):
             self.vector[0] = x1
             self.vector[2] = z1
 
-    def z_rotation(self, angle, copy=True):
+    def z_rotation(self, angle:float, copy:bool=True):
         """
         Rotation of angle around Z axis.
         """
