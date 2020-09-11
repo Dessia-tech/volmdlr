ce589591049e104de12a25acaed2c1db7b09c1a1
Jérémie Chheang <jeremie.chheang@student.isae-supaero.fr>
2020-09-11 16:45:00 +0200

    Taille des points inchangée

--- scripts/Points.py
+++ scripts/Points.py
@@ -8,7 +8,7 @@ from volmdlr import plot_data
 plot_datas = []
 window_size = plot_data.WindowSizeSet(width=2,height=1)
 shape_set = plot_data.PointShapeSet(shape='crux')
-point_size = plot_data.PointSizeSet(size=3)
+point_size = plot_data.PointSizeSet(size=4)
 point_color = plot_data.PointColorSet(color_fill='black', color_stroke='red')
 for i in range(50):
     point = vm.Point2D.random(0,window_size.width,0,window_size.height)
