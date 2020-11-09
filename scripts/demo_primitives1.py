acef6e5a27dd3f11f09ed093a8eab75640877dd2
Steven Masfaraud <masfaraud@dessia.tech>
2020-11-09 15:04:56 +0100

    Little fix on script

--- scripts/demo_primitives1.py
+++ scripts/demo_primitives1.py
@@ -10,9 +10,9 @@ import volmdlr as vm
 import volmdlr.primitives3d as primitives3d
 
 cylinder1 = primitives3d.Cylinder(vm.O3D, vm.X3D,
-                                0.03, 0.02, 'cylinder1')
-cylinder2 = primitives3d.HollowCylinder(vm.Y3D, vm.X3D,
-                                      0.02, 0.06, 0.03,'cylinder2')
+                                0.03, 0.02, name='cylinder1')
+cylinder2 = primitives3d.HollowCylinder(0.1*vm.Y3D, vm.X3D,
+                                      0.02, 0.06, 0.03, name='cylinder2')
 
 model=vm.core.VolumeModel([cylinder1, cylinder2])
 
