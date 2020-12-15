1e4f37c697990346aef1aaa20aef9ca2d4f8c8a1
Steven Masfaraud <masfaraud@dessia.tech>
2020-12-15 11:17:50 +0100

    correcting models init

---
+++ scripts/mesh/electric_machine.py
@@ -0,0 +1,6 @@
+from volmdlr.models import stator_mesher, rotor_mesher
+
+# Mesh from mesher
+#stator_mesh = stator_mesher
+
+# Check mesh
--- volmdlr/models/__init__.py
+++ volmdlr/models/__init__.py
@@ -6,9 +6,5 @@ Created on Fri Feb 28 11:03:00 2020
 @author: masfaraud
 """
 
-from .electric_machine import all_rotor_mesh
-from .electric_machine import stator_mesh
+from .electric_machine import rotor_mesher, stator_mesher
 
-all_rotor_triangle_elements=all_rotor_mesh.generate_mesh(4)
-
-stator_triangle_elements=stator_mesh.generate_mesh(4)
