19f5340b1cdc319a136355d083a0a3d8c0810da8
Steven Masfaraud <masfaraud@dessia.tech>
2021-03-02 15:42:51 +0100

    fix: openrl ploygon points

--- scripts/extrusion2.py
+++ scripts/extrusion2.py
@@ -10,8 +10,11 @@ Created on Sun Mar  5 22:01:35 2017
 
 #import numpy as npy
 import volmdlr as vm
-import volmdlr.primitives3D as primitives3D
-import volmdlr.primitives2D as primitives2D
+import volmdlr.core as vmc
+import volmdlr.edges as vme
+import volmdlr.wires as vmw
+import volmdlr.primitives3d as primitives3d
+import volmdlr.primitives2d as primitives2d
 import math
 
 p1=vm.Point2D(0, 0)
@@ -22,21 +25,21 @@ p5=vm.Point2D(-0.01, 0.05)
 
 #p6=vm.Point2D((0.1,0.3))
 
-l1 = primitives2D.OpenedRoundedLineSegments2D([p1, p2, p3, p4], {2: 0.01})
-l2 = vm.Arc2D(p4, p5, p1)
-c1 = vm.Contour2D([l1, l2])
-c2 = c1.Rotation(vm.Point2D((0,0)),math.pi,True)
-c1.MPLPlot()
-c2.MPLPlot()
+l1 = primitives2d.OpenedRoundedLineSegments2D([p1, p2, p3, p4], {2: 0.01})
+l2 = vme.Arc2D(p4, p5, p1)
+c1 = vmw.Contour2D([l1, l2])
+c2 = c1.rotation(vm.Point2D(0,0), math.pi,True)
+ax = c1.plot()
+c2.plot(ax=ax, color='r')
 #c3 = vm.Contour2D([c1, c2])
 #c3.MPLPlot()
 
 
 
 
-profile = primitives3D.ExtrudedProfile(vm.O3D, vm.Y3D, vm.Z3D, c1, [], 0.1*vm.X3D)
+profile = primitives3d.ExtrudedProfile(vm.O3D, vm.Y3D, vm.Z3D, c1, [], 0.1*vm.X3D)
 
-model = vm.VolumeModel([profile])
+model = vmc.VolumeModel([profile])
 model.babylonjs()
 
 #profile.MPLPlot((0,0,0),(1,0,0),(0,1,0))
