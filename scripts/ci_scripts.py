34f3c9b32e587e74d328f65d7ead7b83900f86d2
Steven Masfaraud <masfaraud@dessia.tech>
2021-03-16 11:38:28 +0100

    chore: moving scripts

--- scripts/ci_scripts.py
+++ scripts/ci_scripts.py
@@ -1,23 +1,18 @@
 
 import os
 
-scripts = ['arcs2D.py', 'arcs3D.py', 'block3d.py', 'simple_shapes.py',
-           'roundedlines.py','polygon2D.py', 'polygon2D_2.py',
-           'primitives/extrusion.py', 'demo2D.py', 'showcases/casing.py', 'sweep.py',
-           'primitives/revolved_profile.py', 'edges/areas_moment_cog_check.py']
+scripts = ['edges/arcs2D.py', 'edges/arcs3D.py', 'showcases/simple_shapes.py',
+           'wires/roundedlines.py','wires/polygon2D.py', 'wires/polygon2D_2.py',
+           'primitives/extrusion.py', 'demo2D.py', 'showcases/casing.py', 'primitives/sweep.py',
+           'primitives/revolved_profile.py', 'edges/areas_moment_cog_check.py',
+           'distance/arc3D_arc3D.py','distance/arc3D_ls3D.py',
+           'primitives/block3d.py']
+
+#  'cyl_cyl.py', 'cyl_pf.py',
+# 'ls3D_ls3D.py', 'sweep_sweep.py', 'tore_cyl.py','tore_pf.py'
+# 'tore_tore.py'
 
 for script_name in scripts:
     print('\n## Executing script {}'.format(script_name))
 
     exec(open(script_name).read())
-
-# TODO: port these commented scripts!
-distance_scripts = ['arc3D_arc3D.py','arc3D_ls3D.py',
-                   #  'cyl_cyl.py', 'cyl_pf.py',
-                   # 'ls3D_ls3D.py', 'sweep_sweep.py', 'tore_cyl.py','tore_pf.py'
-                   # 'tore_tore.py'
-                    ]
-
-for script_name in distance_scripts:
-    print('\n## Executing script {}'.format(script_name))
-    exec(open(os.path.join('distance', script_name)).read())
