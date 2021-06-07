75eb720887d774792dc1ca4eb905dd89492e2807
Mack <mackjoly@gmail.com>
2021-06-07 08:35:35 +0200

    stl_reading

--- scripts/stl/stl_reading.py
+++ scripts/stl/stl_reading.py
@@ -7,40 +7,51 @@ import volmdlr.cloud
 import volmdlr.core
 # import volmdlr as vm
 # import volmdlr.wires as vmw
-# import volmdlr.faces as vmf
+import volmdlr.faces as vmf
 # import volmdlr.edges as vme
 # import matplotlib.pyplot as plt
 
 import os
 
-faces = []
+volum = []
 path = os.getcwd()
 for stl_file in [#'a320.stl',
-                 'a320_ENGINE_RIGHT.stl',
-                 # 'a320_ENGINE_LEFT.stl',
-                 'a320_FAN_RIGHT.stl',
-                 # 'a320_FAN_LEFT.stl',
-                 # 'a320_LEFT_WING.stl',
-                 'a320_RIGHT_WING.stl',
-                 # 'a320_RUDDER.stl',
-                 # 'a320_STABILO_LEFT.stl',
-                 # 'a320_STABILO_RIGHT.stl'
+                  'a320_ENGINE_RIGHT.stl',
+                  # 'a320_ENGINE_LEFT.stl',
+                  'a320_FAN_RIGHT.stl',
+                  # 'a320_FAN_LEFT.stl',
+                  # 'a320_LEFT_WING.stl',
+                  'a320_RIGHT_WING.stl',
+                  # 'a320_RUDDER.stl',
+                  # 'a320_STABILO_LEFT.stl',
+                  # 'a320_STABILO_RIGHT.stl'
                   ]:
     print('start')
     cloud = volmdlr.cloud.PointCloud3D.from_stl(path + "/" + stl_file)
-    cloud_faces = cloud.subdescription_2d()
+    shell = cloud.to_shell()
     
-    volum = volmdlr.core.VolumeModel(cloud_faces)
     print('saving file' + stl_file)
-    volum.save_to_file(stl_file)
+    shell.save_to_file(stl_file)
     # print('len(cloud_faces)', len(cloud_faces))
-    faces.extend(cloud_faces)
+    volum.append(shell)
     print()
     
+volumodel=volmdlr.core.VolumeModel(volum)    
     
     
-    
-volum = volmdlr.core.VolumeModel(faces)
-volum.babylonjs()
+# volum = volmdlr.core.VolumeModel(faces)
+# volum.babylonjs()
         
+# for json_file in ['a320_ENGINE_RIGHT.json',
+                  # 'a320_FAN_RIGHT.json',
+                   # 'a320_RIGHT_WING.json']:
+    # vol = volmdlr.core.VolumeModel.load_from_file(json_file)
+    # faces.extend(vol.primitives)
+# 
+# faces.pop(6230)
+    # 
+# volass = volmdlr.core.VolumeModel(faces)
 
+
+
+# primitive_pb = volass.primitives[6230]
