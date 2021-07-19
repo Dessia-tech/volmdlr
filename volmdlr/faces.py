4c28429b95ce441b580dc0a2019286e5598a845f
Steven Masfaraud <masfaraud@dessia.tech>
2021-07-19 19:46:41 +0200

    fix: point_at_abscissa bspline 2D curve

--- volmdlr/faces.py
+++ volmdlr/faces.py
@@ -1708,9 +1708,8 @@ class BSplineSurface3D(Surface3D):
 
     def linesegment2d_to_3d(self, linesegment2d):
         # TODO: this is a non exact method!
-        l = linesegment2d.length()
-        points = [self.point2d_to_3d(linesegment2d.point_at_abscissa(i / l / 10.)) for i in range(11)]
-
+        l = linesegment2d.length()        
+        points = [self.point2d_to_3d(linesegment2d.point_at_abscissa(i * l/ 10.)) for i in range(11)]
         return [vme.LineSegment3D(p1, p2) \
                 for p1, p2 in zip(points[:-1], points[1:])]
 
@@ -2511,27 +2510,6 @@ class CylindricalFace3D(Face3D):
     def _bounding_box(self):
         theta_min, theta_max, zmin, zmax = self.surface2d.outer_contour.bounding_rectangle()
 
-        # xp = (volmdlr.X3D.dot(self.surface3d.frame.u) * self.surface3d.frame.u
-        #       + volmdlr.X3D.dot(
-        #             self.surface3d.frame.v) * self.surface3d.frame.v)
-        # xp_norm = xp.norm()
-        # if xp_norm != 0:
-        #     xp = xp / xp_norm
-
-        # yp = (volmdlr.Y3D.dot(self.surface3d.frame.u) * self.surface3d.frame.u
-        #       + volmdlr.Y3D.dot(
-        #             self.surface3d.frame.v) * self.surface3d.frame.v)
-        # yp_norm = yp.norm()
-        # if yp_norm != 0:
-        #     yp = yp / yp_norm
-
-        # zp = (volmdlr.Z3D.dot(self.surface3d.frame.u) * self.surface3d.frame.u
-        #       + volmdlr.Z3D.dot(
-        #             self.surface3d.frame.v) * self.surface3d.frame.v)
-        # zp_norm = zp.norm()
-        # if zp_norm != 0:
-        #     zp = zp / zp_norm
-
         lower_center = self.surface3d.frame.origin + zmin * self.surface3d.frame.w
         upper_center = self.surface3d.frame.origin + zmax * self.surface3d.frame.w
 
@@ -2562,14 +2540,6 @@ class CylindricalFace3D(Face3D):
                   (upper_center
                    + xmax * self.surface3d.radius * self.surface3d.frame.u
                    + ymax * self.surface3d.radius * self.surface3d.frame.v)]
-
-
-        # ax = self.plot()
-        # lower_center.plot(ax=ax, color='g')
-        # upper_center.plot(ax=ax, color='b')
-        # for p in points:
-        #     p.plot(ax=ax, color='r')
-        # volmdlr.core.BoundingBox.from_points(points).plot(ax=ax)
         
         return volmdlr.core.BoundingBox.from_points(points)
 
