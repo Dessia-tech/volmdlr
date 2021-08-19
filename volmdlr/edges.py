0aecd13ae40fc259c24d0399d900a644a378386a
MackJoly <61141035+MackJoly@users.noreply.github.com>
2021-08-19 10:04:14 -0400

    adding minimum curvature radius bspline at u

--- volmdlr/edges.py
+++ volmdlr/edges.py
@@ -2685,17 +2685,21 @@ class BSplineCurve3D(Edge):
     def unit_normal_vector(self, abscissa):
         return None
     
-    def minimum_curvature_radius(self, nb_eval: int = 21):
+    def minimum_curvature_radius_at_u(self, u: float):
+        #u should be in the interval [0,1]
         curve = self.curve
+        ders = curve.derivatives(u,3) #3 first derivative
+        c1, c2 = volmdlr.Point3D(*ders[1]), volmdlr.Point3D(*ders[2])
+        denom = c1.cross(c2)
+        r_c = ((c1.norm())**3)/denom.norm()
+        return r_c
+    
+    def minimum_curvature_radius(self, nb_eval: int = 21):
         check = [i/(nb_eval-1) for i in range(nb_eval)]
         
         radius = []
         for u in check :
-            ders = curve.derivatives(u,3) #3 first derivative
-            c1, c2 = volmdlr.Point3D(*ders[1]), volmdlr.Point3D(*ders[2])
-            denom = c1.cross(c2)
-            r_c = ((c1.norm())**3)/denom.norm()
-            radius.append(r_c)
+            radius.append(self.minimum_curvature_radius_at_u(u))
         return radius
 
 class BezierCurve3D(BSplineCurve3D):
