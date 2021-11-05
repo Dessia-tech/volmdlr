dd2148f1ed4f7a83e81065538f4ba70fe0f943bb
Soumiya Bendjebla <bendjebla@dessia.tech>
2021-11-05 09:32:00 +0100

    BsplineSurface3D definition using 'points_fitting_into_bspline_surface' method

--- scripts/faces/bspline_bark.py
+++ scripts/faces/bspline_bark.py
@@ -19,8 +19,9 @@ for z in [0, 0.1, 0.2, 0.3]:
     for x, y in base_points1:
         grid1.append(vm.Point3D(x, y, z))
         
-
-surface1 = vmf.BSplineSurface3D.from_pointgrid(grid1, 4, 4, 2, 2)
+size_u, size_v, degree_u, degree_v = 4, 4, 2, 2
+surface1 = vm.faces.BSplineSurface3D.points_fitting_into_bspline_surface(grid1, size_u, size_v, degree_u, degree_v)
+# surface1 = vmf.BSplineSurface3D.from_pointgrid(grid1, 4, 4, 2, 2)
 face1 = surface1.rectangular_cut(0, 1, 0, 1)
 
 
@@ -34,8 +35,9 @@ for z in [0.05, 0.12, 0.21, 0.28]:
     for x, y in base_points2:
         grid2.append(vm.Point3D(x, y, z))
         
-
-surface2 = vmf.BSplineSurface3D.from_pointgrid(grid2, 4, 4, 2, 2)
+size_u, size_v, degree_u, degree_v = 4, 4, 2, 2
+surface2 = vm.faces.BSplineSurface3D.points_fitting_into_bspline_surface(grid2, size_u, size_v, degree_u, degree_v)
+# surface2 = vmf.BSplineSurface3D.from_pointgrid(grid2, 4, 4, 2, 2)
 face2 = surface2.rectangular_cut(0, 1, 0, 1)
 
 shell = vmf.OpenShell3D([face1, face2])
