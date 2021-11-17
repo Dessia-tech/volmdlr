da24cbd7c9182be3a218a50f6ae5307f1392d106
Ringhausen <ringhausen@dessia.tech>
2021-11-17 16:50:17 +0100

    fix: fix Vector3D.remove_duplicate

--- volmdlr/core_compiled.pyx
+++ volmdlr/core_compiled.pyx
@@ -917,9 +917,15 @@ class Vector3D(Vector):
         
     @classmethod
     def remove_duplicate(cls, points):
-        dict_ = {p.approx_hash() : p for p in points}
-        return list(dict_.values())
-
+        """
+        Don't keep the input list order
+        """
+        points.sort(key=lambda pt: (pt.x, pt.y, pt.z))
+        new_points = [points[0]]
+        for i, point in enumerate(points[1:]):
+            if not point.is_close(points[i]):
+                new_points.append(point)
+        return new_points
 
     @classmethod
     def dict_to_object(cls, dict_):
