560a185f6783020944f0f34d558a9b13ec948a8c
Soumiya Bendjebla <bendjebla@dessia.tech>
2021-12-16 10:05:36 +0100

    scripts: step/read_steps (uncomment step files)

--- scripts/step/read_steps.py
+++ scripts/step/read_steps.py
@@ -6,15 +6,16 @@ import volmdlr.step
 import volmdlr.cloud as vmcd
 
 for step_file in [
-                  # 'tore1.step',
-                  # 'cone1.step',
-                  # 'cone2.step',
-                  # 'cylinder.step',
-                    # 'STEP_test1.stp',
-                  # 'block.step',
-                    # 'iso4162M16x55.step',
-                    'aircraft_engine.step'
-                  ]:
+    'tore1.step',
+    'cone1.step',
+    'cone2.step',
+    'cylinder.step',
+    'block.step',
+
+    # 'STEP_test1.stp',
+    # 'iso4162M16x55.step',
+    # 'aircraft_engine.step'
+  ]:
     print('filename: ', step_file)
     step = volmdlr.step.Step(step_file)
     model = step.to_volume_model()
