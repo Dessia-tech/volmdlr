fe47c859a9586f15cac24aa50c32522a07b2faf5
unknown <joly@dessia.tech>
2022-01-03 12:55:40 -0500

    ancient remove duplicate

--- volmdlr/core_compiled.pyx
+++ volmdlr/core_compiled.pyx
@@ -379,15 +379,17 @@ class Vector2D(Vector):
 
     @classmethod
     def remove_duplicate(cls, points):
-        """
-        Don't keep the input list order
-        """
-        input_points = sorted(points, key=lambda pt: (pt.x, pt.y))
-        new_points = [points[0]]
-        for i, point in enumerate(input_points[1:]):
-            if not point.is_close(new_points[-1]):
-                new_points.append(point)
-        return new_points
+        # """
+        # Don't keep the input list order
+        # """
+        # input_points = sorted(points, key=lambda pt: (pt.x, pt.y))
+        # new_points = [points[0]]
+        # for i, point in enumerate(input_points[1:]):
+        #     if not point.is_close(new_points[-1]):
+        #         new_points.append(point)
+        # return new_points
+        dict_ = {p.approx_hash() : p for p in points}
+        return list(dict_.values())
 
     def norm(self):
         """
@@ -937,12 +939,14 @@ class Vector3D(Vector):
         """
         Don't keep the input list order
         """
-        input_points = sorted(points, key=lambda pt: (pt.x, pt.y, pt.z))
-        new_points = [points[0]]
-        for i, point in enumerate(input_points[1:]):
-            if not point.is_close(new_points[-1]):
-                new_points.append(point)
-        return new_points
+        # input_points = sorted(points, key=lambda pt: (pt.x, pt.y, pt.z))
+        # new_points = [points[0]]
+        # for i, point in enumerate(input_points[1:]):
+        #     if not point.is_close(new_points[-1]):
+        #         new_points.append(point)
+        # return new_points
+        dict_ = {p.approx_hash() : p for p in points}
+        return list(dict_.values())
 
     @classmethod
     def dict_to_object(cls, dict_):
