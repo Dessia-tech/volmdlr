c00275e071ca9e470f407f890590beb7e9e95bf6
unknown <joly@dessia.tech>
2022-01-06 13:12:28 -0500

    to dict triangles

--- volmdlr/faces.py
+++ volmdlr/faces.py
@@ -3938,8 +3938,6 @@ class Triangle3D(PlaneFace3D):
         dict_['point1'] = self.point1.to_dict()
         dict_['point2'] = self.point2.to_dict()
         dict_['point3'] = self.point3.to_dict()
-        dict_['name'] = self.name
-        dict_['object_class'] = 'volmdlr.faces.Triangle3D'
         return dict_
 
     # def to_dict(self):
