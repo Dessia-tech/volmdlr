5605f9b32cc16dfdbd23de14caf274200900c44b
Steven Masfaraud <masfaraud@dessia.tech>
2022-01-06 10:28:49 +0100

    fix: bash code quality

--- code_quality.sh
+++ code_quality.sh
@@ -1,4 +1,6 @@
-pylama -l radon volmdlr
-if [[ $(pylama -l radon volmdlr) ]];
-	then echo "Error in code quality check">&2;
+#!/bin/bash
+cq_result=$(radon cc --min D volmdlr)
+echo $cq_result
+if [[ "$cq_result" ]];
+	then echo "Error in code quality check, run radon to simplify functions">&2;
 fi;
