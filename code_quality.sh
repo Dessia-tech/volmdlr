0015b658e0d06fb286f77b04956133035e1a720a
Steven Masfaraud <masfaraud@dessia.tech>
2022-01-06 10:33:52 +0100

    fix: ci

--- code_quality.sh
+++ code_quality.sh
@@ -2,5 +2,8 @@
 cq_result=$(radon cc --min D volmdlr)
 echo $cq_result
 if [[ "$cq_result" ]];
-	then echo "Error in code quality check, run radon to simplify functions">&2;
+  then 
+	  echo "Error in code quality check, run radon to simplify functions">&2;
+	  exit 64;
+	
 fi;
