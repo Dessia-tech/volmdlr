58629d9b812bc6e03e139e2fbf4bfef95fb1da22
sbendjebla <bendjebla-soumiya@outlook.fr>
2022-01-25 15:19:06 +0100

    Contour2D: point_over_contour (Unused argument)

--- volmdlr/wires.py
+++ volmdlr/wires.py
@@ -995,7 +995,7 @@ class Contour2D(Contour, Wire2D):
     def point_over_contour(self, point, abs_tol=1e-7):
         belongs = False
         for primitive in self.primitives:
-            if primitive.point_belongs(point, abs_tol=1e-7):
+            if primitive.point_belongs(point, abs_tol):
                 belongs = True
         return belongs
 
