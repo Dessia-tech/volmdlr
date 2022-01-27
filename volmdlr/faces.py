594a887bbd4feb33a49ebac21b04d56aa91bee31
Ringhausen <ringhausen@dessia.tech>
2022-01-27 18:30:57 +0100

    fix: keyword edge_details added to plot methods

--- volmdlr/faces.py
+++ volmdlr/faces.py
@@ -3753,6 +3753,7 @@ class Face3D(volmdlr.core.Primitive3D):
     def plot(self, ax=None, color='k', alpha=1, edge_details=False):
         if not ax:
             ax = plt.figure().add_subplot(111, projection='3d')
+        print(self.outer_contour3d)
         self.outer_contour3d.plot(ax=ax, color=color, alpha=alpha,
                                   edge_details=edge_details)
         [contour3d.plot(ax=ax, color=color, alpha=alpha,
