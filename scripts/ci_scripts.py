f9285e92ff6f1991986b4ba034cf24d15d388a31
Steven Masfaraud <masfaraud@dessia.tech>
2022-02-04 18:55:30 +0100

    script cleaning

--- scripts/ci_scripts.py
+++ scripts/ci_scripts.py
@@ -1,18 +1,21 @@
 
-import os
-import matplotlib.pyplot as plt
 
 scripts = [
+            # Core.py
+            'core/points.py',
+            # geometry
+            'geometry.py',
             # Edges
             'edges/arcs2D.py',
             'edges/arcs3D.py',
             'edges/bspline.py',
+            'edges/bspline2.py',
             'edges/areas_moment_cog_check.py',
+            'edges/lines2d.py',
             # Wires
             'wires/roundedlines.py',
             'wires/polygon2D.py',
             'wires/triangle2D.py',
-            'demo2D.py',
             # Primitives
             'primitives/extrusion.py', 
             'primitives/sweep.py',
@@ -20,6 +23,7 @@ scripts = [
             'primitives/block3d.py',
             'primitives/sphere_to_point.py',
             'primitives/cone.py',
+            'primitives/cylinders.py',
             # Faces
             'faces/triangle3d.py',
             'faces/bspline.py',
@@ -51,7 +55,5 @@ scripts = [
 
 for script_name in scripts:
     print('\n## Executing script {}'.format(script_name))
-    plt.show()
-    a = input('ok')
 
     exec(open(script_name).read())
