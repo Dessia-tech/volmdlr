f1a106e6844b048551facf3057bdaeb1e432c12a
WirajanDASILVA <wirajan.klebson@gmail.com>
2022-02-08 14:51:56 -0300

    fix ci_scripts.py

--- scripts/ci_scripts.py
+++ scripts/ci_scripts.py
@@ -2,41 +2,41 @@
 
 scripts = [
             # Core.py
-            # 'core/points.py',
-            # # geometry
-            # 'geometry.py',
-            # # Edges
-            # 'edges/arcs2D.py',
-            # 'edges/arcs3D.py',
-            # 'edges/bspline.py',
-            # 'edges/bspline2.py',
-            # 'edges/areas_moment_cog_check.py',
-            # 'edges/lines2d.py',
-            # # Wires
-            # 'wires/roundedlines.py',
-            # 'wires/polygon2D.py',
-            # 'wires/triangle2D.py',
-            # # Primitives
-            # 'primitives/extrusion.py',
-            # 'primitives/sweep.py',
-            # 'primitives/revolved_profile.py',
-            # 'primitives/block3d.py',
-            # 'primitives/sphere_to_point.py',
-            # 'primitives/cone.py',
-            # 'primitives/cylinders.py',
-            # # Faces
-            # 'faces/triangle3d.py',
-            # 'faces/bspline.py',
-            # 'faces/bspline_bark.py',
-            # 'faces/union.py',
-            # 'faces/blocks_set_operations.py',
-            # # Cloud
-            # 'cloud/sewing_two_polygons.py',
-            # 'cloud/sewing_stl.py',
-            # # Contours
-            # # 'contours/contour_split.py',
-            # 'contours/contour_merge_with_1.py',
-            # 'contours/contour_merge_with_2.py',
+            'core/points.py',
+            # geometry
+            'geometry.py',
+            # Edges
+            'edges/arcs2D.py',
+            'edges/arcs3D.py',
+            'edges/bspline.py',
+            'edges/bspline2.py',
+            'edges/areas_moment_cog_check.py',
+            'edges/lines2d.py',
+            # Wires
+            'wires/roundedlines.py',
+            'wires/polygon2D.py',
+            'wires/triangle2D.py',
+            # Primitives
+            'primitives/extrusion.py',
+            'primitives/sweep.py',
+            'primitives/revolved_profile.py',
+            'primitives/block3d.py',
+            'primitives/sphere_to_point.py',
+            'primitives/cone.py',
+            'primitives/cylinders.py',
+            # Faces
+            'faces/triangle3d.py',
+            'faces/bspline.py',
+            'faces/bspline_bark.py',
+            'faces/union.py',
+            'faces/blocks_set_operations.py',
+            # Cloud
+            'cloud/sewing_two_polygons.py',
+            'cloud/sewing_stl.py',
+            # Contours
+            # 'contours/contour_split.py',
+            'contours/contour_merge_with_1.py',
+            'contours/contour_merge_with_2.py',
             'contours/cut_by_line.py',
             # Steps
             'read_steps.py',
