026e6d0543d85472675018dca1b89594d7bfab01
sbendjebla <bendjebla-soumiya@outlook.fr>
2022-02-16 16:16:01 +0100

    BsplineCurve3D: point_belongs (use z.cost)

--- volmdlr/edges.py
+++ volmdlr/edges.py
@@ -2967,7 +2967,7 @@ class BSplineCurve3D(Edge, volmdlr.core.Primitive3D):
             
         for x0 in x_init: 
             z = scp.optimize.least_squares(f, x0=x0, bounds=([0,1]))
-            if z.fun < abs_tol: 
+            if z.cost < abs_tol:
                 return True
         return False
 
