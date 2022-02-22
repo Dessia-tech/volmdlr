e36df3b8b1861c5d3b7575fe2d175a1fd2fd93e5
Steven Masfaraud <masfaraud@dessia.tech>
2022-02-22 20:11:27 +0100

    ci: script sort times

--- scripts/ci_scripts.py
+++ scripts/ci_scripts.py
@@ -65,15 +65,30 @@ for script_name in scripts:
     if not os.path.isfile(script_name):
         raise FileNotFoundError(f'Script {script_name} does not exists in CI scripts')
 
+# Executing scripts
 print('Executing scripts for CI:')
 total_time = time.time()
-# Executing scripts
+top_level_dir = os.getcwd()
+times = {}
 for script_name in scripts:
+    # Reset dir
+    os.chdir(top_level_dir)
+    # Change cwd
+    if '/' in script_name:
+        script_folder = '/'.join(script_name.split('/')[:-1])
+        if script_folder:
+            script_folder = os.path.join(top_level_dir, script_folder)
+            os.chdir(script_folder)
+    file_name = script_name.split('/')[-1]
     t = time.time()
-    with open(script_name, 'r', encoding='utf-8') as script:
+    with open(file_name, 'r', encoding='utf-8') as script:
         exec(script.read())
     t = time.time() - t
-    print(f'\t-> script{script_name} executed in {round(t, 3)} seconds ')
+    times[script_name] = t
+
+print('Computation times:')
+for script_name, t in sorted(times.items(), key=lambda x:x[1]):
+    print(f'\t-> script{script_name}: {round(t, 3)} seconds ')
     
 total_time = time.time() - total_time
 print(f'Total time for CI scripts: {total_time}')
