c50faede64ac9d99f18f6d78d48ab173e7543a6f
sbendjebla <bendjebla-soumiya@outlook.fr>
2022-03-02 13:41:55 +0100

    script: contour_cut_by_wire.py (update)

--- scripts/contours/contour_cut_by_wire.py
+++ scripts/contours/contour_cut_by_wire.py
@@ -35,21 +35,13 @@ contours = contour.cut_by_wire(wire)
 
 fig, axs = plt.subplots(1, 3)
 
-contour.plot(ax=axs[0])
-for prim in wire.primitives:
-    prim.plot(ax=axs[0], width=2, color='r')
-axs[0].set_title("Initial Contour2d + Wire2d")
-
-
-contour.plot(ax=axs[1])
-wire.plot(ax=axs[1])
-for prim in contours[0].primitives:
-    prim.plot(ax=axs[1], width=2, color='g')
-axs[1].set_title("1st Cutted Contour2d 'green'")
-
-
-contour.plot(ax=axs[2])
-wire.plot(ax=axs[2])
-for prim in contours[1].primitives:
-    prim.plot(ax=axs[2], width=2, color='b')
-axs[2].set_title("2nd Cutted Contour2d 'blue'")
+titles = ["Initial Contour2d + Wire2d", "1st Cutted Contour2d 'green'", "2nd Cutted Contour2d 'blue'"]
+colors = ['g', 'b']
+for i in range(len(axs)):
+    contour.plot(ax=axs[i])
+    for prim in wire.primitives:
+        prim.plot(ax=axs[i], width=2, color='r')
+    axs[i].set_title(titles[i])
+    if i !=0:
+        for prim in contours[i-1].primitives:
+            prim.plot(ax=axs[i], width=2, color=colors[i-1])
