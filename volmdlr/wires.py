996fb4db225c1edd35700ad1d7386fd0fae6b8aa
Soumiya Bendjebla <bendjebla@dessia.tech>
2022-03-10 13:31:06 +0100

    Wire: is_ordered (update)

--- volmdlr/wires.py
+++ volmdlr/wires.py
@@ -290,7 +290,7 @@ class Wire:
         '''
 
         for primitive_1, primitive_2 in zip(self.primitives, self.primitives[1:]):
-            if primitive_1.point_distance(primitive_2) < tol:
+            if primitive_1.start.point_distance(primitive_2.end) < tol:
                 continue
             else:
                 return False
