e7e0bdcb9a36a4807160f8cc775f7c676c8d7694
Soumiya Bendjebla <bendjebla@dessia.tech>
2022-03-17 15:01:24 +0100

    Scripts: triangularisation.py (update)

--- scripts/triangularisation.py
+++ scripts/triangularisation.py
@@ -25,13 +25,13 @@ contour = vm.wires.Contour2D(vm.wires.ClosedPolygon2D(p).primitives)
 pts = vm.wires.Circle2D(vm.O2D, radius=0.05).tessellation_points(resolution=40)
 inner_contour = vm.wires.ClosedPolygon2D(pts)
 
-# %% Surface2D + inners
+# %% 1/ Surface2D + inners
 
 surface = volmdlr.faces.Surface2D(outer_contour=contour, inner_contours=[inner_contour])
 
-# %% Triangularisation
+# %%% Triangularisation
 
-x_density, y_density = 5, 5
+x_density, y_density = 3,3
 
 triangularisation = surface.triangularisation_2(x_density, y_density)
 
@@ -39,19 +39,11 @@ ax =surface.plot()
 for element in triangularisation:
     element.plot(ax, 'b')
 
-
-primitives = [vm.edges.LineSegment2D(vm.Point2D(-0.35, -0.1), vm.Point2D(-0.1, 0)),
-              vm.edges.LineSegment2D(vm.Point2D(-0.1, 0), vm.Point2D(0.2, 0.2)),
-              vm.edges.LineSegment2D(vm.Point2D(0.2, 0.2), vm.Point2D(0.3, 0.3))]
-
-wire = vm.wires.Wire2D(primitives)
-
-
-# %% Surface2D + not inners
+# %% 2/ Surface2D + not inners
 
 surface = volmdlr.faces.Surface2D(outer_contour=contour, inner_contours=[])
 
-# %% Triangularisation
+# %%% Triangularisation
 
 triangularisation = surface.triangularisation_2(x_density, y_density)
 
