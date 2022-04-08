20267321e40fac8a4a5f55d8f2d6885b2dcf1db6
sbendjebla <bendjebla-soumiya@outlook.fr>
2022-04-08 12:17:06 +0200

    Grid2D (order the methods alphabetically)

--- volmdlr/grid.py
+++ volmdlr/grid.py
@@ -21,6 +21,65 @@ class Grid2D(DessiaObject):
         self.direction = direction
         DessiaObject.__init__(self, name=name)
 
+    def displacement_compared_to(self, initial_grid2d):
+        """
+        compute the deformation/displacement (dx,dy) of a grid2d based on an another grid2d
+
+        Parameters
+        ----------
+        grid2d : Grid2D
+
+        """
+
+        points_2d = initial_grid2d.points
+        points_2d_deformed = self.points
+
+        displacement = npy.ones(shape=(len(points_2d), 2))  #Grid2D points displacement
+        for i in range(0, len(displacement)):
+            displacement[i][0] = points_2d_deformed[i][0] - points_2d[i][0]
+            displacement[i][1] = points_2d_deformed[i][1] - points_2d[i][1]
+
+        return displacement
+
+    def find_direction_index(self, direction_axis: str):
+        """
+        find the index of a given direction_axis
+
+        Parameters
+        ----------
+        direction_axis : str
+            'x' OR 'y'
+
+        Returns
+        -------
+        index : int
+
+        """
+
+        try:
+            index = self.direction.index('+' + direction_axis)
+        except ValueError:
+            index = self.direction.index('-' + direction_axis)
+
+        return index
+
+    @classmethod
+    def from_points(cls, points, points_dim_1, direction):
+        """
+        define a Grid2D given a list of points, number of points along the 1st dimension, and a direction
+
+        Parameters
+        ----------
+        points : list[volmdlr.Point2D]
+        points_dim1 : int
+        direction : List[str]
+
+        """
+
+        lists_points = [points[i:i+points_dim_1] for i in range(0, len(points), points_dim_1)]
+
+        return cls(lists_points, direction)
+
     @classmethod
     def from_properties(cls, x_limits, y_limits, points_nbr,
                         direction=['+x', '+y']):
@@ -150,6 +209,41 @@ class Grid2D(DessiaObject):
 
         return cls(lists_points=grid2d, direction=direction)
 
+    def grid_pattern(self):
+        """
+        define a list of quadrilateral polygons defined based on Grid2d points
+
+        Returns
+        -------
+        quadrilateral_polygons : list[volmdlr.wires.ClosedPolygon2D]
+
+        """
+
+        quadrilateral_polygons = []
+        length_1, length_2 = len(self.lists_points[0]), len(self.lists_points)
+        for i in range(0, length_1 - 1):
+            for j in range(0, length_2 - 1):
+                quadrilateral_polygons.append(volmdlr.wires.ClosedPolygon2D(
+                    (self.lists_points[i][j],
+                     self.lists_points[i + 1][j],
+                     self.lists_points[i + 1][j + 1],
+                     self.lists_points[i][j + 1])))
+
+        return quadrilateral_polygons
+
+    @property
+    def limits_xy(self):
+        """
+        find the limits (min, max) of points along x & y direction_axis
+        """
+
+        x_limits = (self.lists_points[0][0].x, self.lists_points[-1][-1].x)
+        x_min, x_max = min(x_limits), max(x_limits)
+        y_limits = (self.lists_points[0][0].y, self.lists_points[-1][-1].y)
+        y_min, y_max = min(y_limits), max(y_limits)
+
+        return ((x_min, x_max), (y_min, y_max))
+
     # @property
     # def x_limits(self):
     #     """
@@ -173,17 +267,36 @@ class Grid2D(DessiaObject):
     #     return (y_min, y_max)
 
     @property
-    def limits_xy(self):
+    def points(self):
         """
-        find the limits (min, max) of points along x & y direction_axis
+        return all the points in lists_points in just one list
+
+        Returns
+        -------
+        points: list[volmdlr.Point2D]
+
         """
 
-        x_limits = (self.lists_points[0][0].x, self.lists_points[-1][-1].x)
-        x_min, x_max = min(x_limits), max(x_limits)
-        y_limits = (self.lists_points[0][0].y, self.lists_points[-1][-1].y)
-        y_min, y_max = min(y_limits), max(y_limits)
+        points = []
+        for list_point in self.lists_points:
+            points.extend(list_point)
+        return points
 
-        return ((x_min, x_max), (y_min, y_max))
+    @property
+    def points_xy(self):
+        """
+        find how many points there are along x & y direction_axis
+        """
+
+        index = self.find_direction_index(direction_axis='x')
+        if index == 0:
+            points_x = len(self.lists_points[0])
+            points_y = len(self.lists_points)
+        else:
+            points_x = len(self.lists_points)
+            points_y = len(self.lists_points[0])
+
+        return (points_x, points_y)
 
     # @property
     # def points_x(self):
@@ -212,117 +325,3 @@ class Grid2D(DessiaObject):
     #         points_y = len(self.lists_points)
 
     #     return points_y
-
-    @property
-    def points_xy(self):
-        """
-        find how many points there are along x & y direction_axis
-        """
-
-        index = self.find_direction_index(direction_axis='x')
-        if index == 0:
-            points_x = len(self.lists_points[0])
-            points_y = len(self.lists_points)
-        else:
-            points_x = len(self.lists_points)
-            points_y = len(self.lists_points[0])
-
-        return (points_x, points_y)
-
-    def find_direction_index(self, direction_axis: str):
-        """
-        find the index of a given direction_axis
-
-        Parameters
-        ----------
-        direction_axis : str
-            'x' OR 'y'
-
-        Returns
-        -------
-        index : int
-
-        """
-
-        try:
-            index = self.direction.index('+' + direction_axis)
-        except ValueError:
-            index = self.direction.index('-' + direction_axis)
-
-        return index
-
-    def grid_pattern(self):
-        """
-        define a list of quadrilateral polygons defined based on Grid2d points
-
-        Returns
-        -------
-        quadrilateral_polygons : list[volmdlr.wires.ClosedPolygon2D]
-
-        """
-
-        quadrilateral_polygons = []
-        length_1, length_2 = len(self.lists_points[0]), len(self.lists_points)
-        for i in range(0, length_1 - 1):
-            for j in range(0, length_2 - 1):
-                quadrilateral_polygons.append(volmdlr.wires.ClosedPolygon2D(
-                    (self.lists_points[i][j],
-                     self.lists_points[i + 1][j],
-                     self.lists_points[i + 1][j + 1],
-                     self.lists_points[i][j + 1])))
-
-        return quadrilateral_polygons
-
-    @property
-    def points(self):
-        """
-        return all the points in lists_points in just one list
-
-        Returns
-        -------
-        points: list[volmdlr.Point2D]
-
-        """
-
-        points = []
-        for list_point in self.lists_points:
-            points.extend(list_point)
-        return points
-
-    @classmethod
-    def from_points(cls, points, points_dim_1, direction):
-        """
-        define a Grid2D given a list of points, number of points along the 1st dimension, and a direction
-
-        Parameters
-        ----------
-        points : list[volmdlr.Point2D]
-        points_dim1 : int
-        direction : List[str]
-
-        """
-
-        lists_points = [points[i:i+points_dim_1] for i in range(0, len(points), points_dim_1)]
-
-        return cls(lists_points, direction)
-
-    def displacement_compared_to(self, initial_grid2d):
-        """
-        compute the deformation/displacement (dx,dy) of a grid2d based on an another grid2d
-
-        Parameters
-        ----------
-        grid2d : Grid2D
-
-        """
-
-        points_2d = initial_grid2d.points
-        points_2d_deformed = self.points
-
-        displacement = npy.ones(shape=(len(points_2d), 2))  #Grid2D points displacement
-        for i in range(0, len(displacement)):
-            displacement[i][0] = points_2d_deformed[i][0] - points_2d[i][0]
-            displacement[i][1] = points_2d_deformed[i][1] - points_2d[i][1]
-
-        return displacement
-
