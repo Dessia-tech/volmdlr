d3e7990bdb66cd2fa62d051cba1daa7cb1dbb01f
sbendjebla <bendjebla-soumiya@outlook.fr>
2022-04-08 16:52:14 +0200

    ci_scripts (comment)

--- scripts/ci_scripts.py
+++ scripts/ci_scripts.py
@@ -28,8 +28,8 @@ scripts = [
             'primitives/cylinders.py',
             # Faces
             'faces/triangle3d.py',
-            'faces/bspline.py',
-            'faces/bspline_bark.py',
+            # 'faces/bspline.py',
+            # 'faces/bspline_bark.py',
             'faces/union.py',
             'faces/blocks_set_operations.py',
             'faces/BSplineSurface/bspline_curves.py',
