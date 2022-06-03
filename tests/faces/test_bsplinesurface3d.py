b69e3754016c3b45701debcf6fcce940e7373176
WirajanDASILVA <wirajan.klebson@gmail.com>
2022-06-03 11:55:02 -0300

    change assertEqual to assertAlmostEqual

--- tests/faces/test_bsplinesurface3d.py
+++ tests/faces/test_bsplinesurface3d.py
@@ -13,8 +13,8 @@ class TestBSplineSurface3D(unittest.TestCase):
         grid2d = volmdlr.grid.Grid2D.from_properties((0, 1), (0, 1), (10, 10))
         contour2d_dim = bspline_surfaces.bspline_surface_2.contour2d_parametric_to_dimension(contour2d, grid2d)
         self.assertEqual(len(contour2d_dim.primitives), 4)
-        self.assertEqual(contour2d_dim.area(), 18.12438798529036)
-        self.assertEqual(contour2d_dim.length(), 16.816547325087043)
+        self.assertAlmostEqual(contour2d_dim.area(), 18.12438798529036)
+        self.assertAlmostEqual(contour2d_dim.length(), 16.816547325087043)
 
 
 if __name__ == '__main__':
