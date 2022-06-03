a5a79545cae6ccd7967e95426fd65ad262bd78a3
WirajanDASILVA <wirajan.klebson@gmail.com>
2022-06-03 11:26:11 -0300

    add len primitives check to unittest

--- tests/faces/test_bsplinesurface3d.py
+++ tests/faces/test_bsplinesurface3d.py
@@ -12,6 +12,7 @@ class TestBSplineSurface3D(unittest.TestCase):
         contour2d = bspline_surfaces.bspline_surface_2.contour3d_to_2d(bspline_face.outer_contour3d)
         grid2d = volmdlr.grid.Grid2D.from_properties((0, 1), (0, 1), (10, 10))
         contour2d_dim = bspline_surfaces.bspline_surface_2.contour2d_parametric_to_dimension(contour2d, grid2d)
+        self.assertEqual(len(contour2d_dim.primitives), 4)
         self.assertEqual(contour2d_dim.area(), 18.12438798529036)
         self.assertEqual(contour2d_dim.length(), 16.816547325087043)
 
