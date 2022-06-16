290b3135ba097efdd8aa7a4310b9fd46ebddf1af
Soumiya Bendjebla <bendjebla@dessia.tech>
2022-06-16 17:29:40 +0200

    scripts/mesh/read_msh_file.py

---
+++ scripts/mesh/read_msh_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 16 2022
+
+@author: s.bendjebla
+"""
+
+import volmdlr.gmsh
+
+# %%
+file_path = 'model.msh'
+
+gmsh = volmdlr.gmsh.Gmsh.from_file(file_path)
+
+mesh = gmsh.define_mesh()
+
+mesh.plot()
