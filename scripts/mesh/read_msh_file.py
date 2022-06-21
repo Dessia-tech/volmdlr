c0e95096bafdbf83d790226eae35917705d02416
Soumiya Bendjebla <bendjebla@dessia.tech>
2022-06-21 17:24:18 +0200

    scripts/mesh/read_msh_file.py (update)

--- scripts/mesh/read_msh_file.py
+++ scripts/mesh/read_msh_file.py
@@ -9,11 +9,20 @@ Created on Thu Jun 16 2022
 import volmdlr
 import volmdlr.gmsh
 
-# %%
-file_path = '/home/bendjebla/Python/DessIA/volmdlr/scripts/step/block.msh'
+# %% 2D
+
+file_path = 'model.msh'
 
 gmsh = volmdlr.gmsh.Gmsh.from_file(file_path)
 
-mesh = gmsh.define_mesh()
+mesh = gmsh.define_triangular_element_mesh()
 
 mesh.plot()
+
+# %% 3D
+
+file_path = 'block.msh'
+
+gmsh = volmdlr.gmsh.Gmsh.from_file(file_path)
+
+mesh = gmsh.define_tetrahedron_element_mesh()
