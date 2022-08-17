4b10865189e8989776800167dd5455f4d69e9303
gibertini <pierre.gibertini@dessia.tech>
2022-08-17 17:06:18 +0200

    new test cases

--- scripts/collisions/cylinders_collision.py
+++ scripts/collisions/cylinders_collision.py
@@ -15,47 +15,73 @@ cylinders = [
         axis=vm.Vector3D(1, 0, 0),
         radius=0.01,
         length=0.1,
+        color=(1, 0, 0),
     ),
     Cylinder(
         position=vm.Point3D(0, 0.05, 0),
         axis=vm.Vector3D(1, 1, 0),
         radius=0.005,
         length=0.1,
+        color=(0, 1, 0),
     ),
     Cylinder(
         position=vm.Point3D(0, 0.159, 0),
         axis=vm.Vector3D(1, 1, 0),
         radius=0.02,
         length=0.1,
+        color=(0, 0, 1),
+    ),
+    Cylinder(
+        position=vm.Point3D(0, 0.1, 0.016),
+        axis=vm.Vector3D(0, 1, 0),
+        radius=0.01,
+        length=0.1,
+        color=(1, 0, 1),
     ),
 ]
-
-cylinders[0].color = (1, 0, 0)
-cylinders[1].color = (0, 1, 0)
-cylinders[2].color = (0, 0, 1)
 
 volume_model = vm.core.VolumeModel(cylinders)
 volume_model.babylonjs()
 
-print("Collision detection methods\n")
+print("Collision detection methods")
 start = time()
 
 print(
     f"""\nRed & green:
-min distance is {cylinders[0].min_distance_to_other_cylinder(cylinders[1])}m, 
+min distance computed is {cylinders[0].min_distance_to_other_cylinder(cylinders[1])}m, 
 collision: {cylinders[0].is_intersecting_other_cylinder(cylinders[1])}"""
 )
 
 print(
     f"""\nRed & blue:
-min distance is {cylinders[0].min_distance_to_other_cylinder(cylinders[2])}m, 
+min distance computed is {cylinders[0].min_distance_to_other_cylinder(cylinders[2])}m, 
 collision: {cylinders[0].is_intersecting_other_cylinder(cylinders[2])}"""
 )
 
 print(
     f"""\nGreen & blue:
-min distance is {cylinders[1].min_distance_to_other_cylinder(cylinders[2])}m, 
+min distance computed is {cylinders[1].min_distance_to_other_cylinder(cylinders[2])}m, 
 collision: {cylinders[1].is_intersecting_other_cylinder(cylinders[2])}"""
 )
 
-print(f"\nCollision detection computing time: {time() - start}s")
+print(
+    f"""\nRed & purple:
+min distance computed is {cylinders[0].min_distance_to_other_cylinder(cylinders[3])}m, 
+collision: {cylinders[0].is_intersecting_other_cylinder(cylinders[3])}"""
+)
+
+print(
+    f"""\nGreen & purple:
+min distance computed is {cylinders[1].min_distance_to_other_cylinder(cylinders[3])}m, 
+collision: {cylinders[1].is_intersecting_other_cylinder(cylinders[3])}"""
+)
+
+print(
+    f"""\nPurple & blue:
+min distance computed is {cylinders[3].min_distance_to_other_cylinder(cylinders[2])}m, 
+collision: {cylinders[3].is_intersecting_other_cylinder(cylinders[2])}"""
+)
+
+end = time()
+print(f"\nTotal collision detection computation time: {end - start}s")
+print(f"Time per collision: {(end - start)/6}s")
