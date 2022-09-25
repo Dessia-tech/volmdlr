edce01045ab71c29356f74264723e7ac11af35dd
younesdessia <younes.belabid@dessia.io>
2022-09-25 22:09:56 +0100

    update read step : add new files to liste

--- scripts/step/read_steps.py
+++ scripts/step/read_steps.py
@@ -4,22 +4,30 @@ import io
 import os
 # import volmdlr as vm
 import volmdlr.step
+
 # import volmdlr.cloud as vmcd
 
 for step_file in [
     'tore1.step',
+    'bracket.STEP',
+    'bracket.stp',
+    'Bracket_v64.STEP',
+    'bracketv3.stp',
+    'FinalDesign-UWMadison.STEP',
+    'GE jet engine bracket v.3m.step',
+    'iter8finaldesign.stp',
     'cone1.step',
     'cone2.step',
     'cylinder.step',
     'block.step',
     # '2_bspline_faces.stp'# Uncomment when bug of delta fixed!
-  ]:
+]:
     print('Reading step file: ', step_file)
     # filepath = os.path.join('step', step_file)
     step = volmdlr.step.Step.from_file(filepath=step_file)
     model = step.to_volume_model()
     assert len(model.primitives) > 0.
-    model.to_step(step_file+'_reexport')
+    model.to_step(step_file + '_reexport')
     model.babylonjs()
 
     file_io = io.FileIO(step_file, 'r')
@@ -29,7 +37,7 @@ for step_file in [
     model.to_step(step_file + '_reexport')
 
     model2 = model.copy()
-    
+
     # model2 = model.copy()
     # assert model == model2
 
