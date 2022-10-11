3507c0fd6b55cba4ca60725e9de6f206ba9a8502
Soumiya Bendjebla <bendjebla@dessia.tech>
2022-10-11 12:25:57 +0200

    Scripts: export bspline_face to step file (update)

--- scripts/faces/export_to_step.py
+++ scripts/faces/export_to_step.py
@@ -6,16 +6,27 @@ Created on Mon Feb 14 2022
 @author: s.bendjebla
 """
 
+# %% Librairies
+
+from volmdlr.models import bspline_surfaces
 import volmdlr.core
 import volmdlr.step
 
+# %% BsplineFaces3D
 
-#Export
-filepath = '../faces/volume_model.json'
-model = volmdlr.core.VolumeModel.load_from_file(filepath)
+bspline_faces = [bspline_surfaces.bspline_surface_2.rectangular_cut(0,1,0,1)]
 
-#Check
-filepath = '/home/bendjebla/Téléchargements/step.step'
-step = volmdlr.step.Step.from_file(filepath=filepath)
-model = step.to_volume_model()    
-model.babylonjs()
+# %% Export
+
+model = volmdlr.core.VolumeModel(bspline_faces)
+
+model.to_step('model_to_step.stp')
+
+# %% Check
+
+filepath = 'model_to_step.stp'
+
+step = volmdlr.step.Step.from_file(filepath)
+model_imported = step.to_volume_model()
+
+assert model_imported == model
