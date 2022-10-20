f9dc0477d0f36d8534899ec765d357866f381638
Steven Masfaraud <masfaraud@dessia.tech>
2022-10-20 16:53:51 +0200

    enhance script

--- scripts/primitives/extrusion3.py
+++ scripts/primitives/extrusion3.py
@@ -21,14 +21,19 @@ delta_angle = 2*math.pi/number_holes
 inner_circle = vmw.Circle2D(vm.O2D, 0.04)
 first_circle = vmw.Circle2D(vm.Point2D(0, 0.05), 0.005)
 circles = [inner_circle, first_circle]
+
+extrusion_vector = vm.X3D*0.1
+
 for i in range(1, number_holes):
     circles.append(first_circle.rotation(vm.O2D, i*delta_angle))
 
 
 extrusion = ExtrudedProfile(vm.O3D, vm.Y3D, vm.Z3D, outer_circle,
-                                            circles, vm.X3D*0.1)
+                                            circles, extrusion_vector)
 
-print(extrusion.volume())
+inner_circles_area = sum([c.area() for c in circles])
+assert math.isclose(extrusion.volume(), (outer_circle.area() - inner_circles_area)*extrusion_vector.norm())
+
 model = vmc.VolumeModel([extrusion])
 model.babylonjs()
 model._check_platform()
