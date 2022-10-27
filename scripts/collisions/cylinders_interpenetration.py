b7193259b43e6f6e6216987ed5bc97c28da4219f
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2022-10-27 12:26:44 +0200

    decrease number of discretization points

--- scripts/collisions/cylinders_interpenetration.py
+++ scripts/collisions/cylinders_interpenetration.py
@@ -20,8 +20,8 @@ cylinders = [
     Cylinder(
         position=vm.Point3D(0.05, 0, 0),
         axis=vm.Vector3D(0, 1, 0),
-        radius=0.02,
-        length=0.1,
+        radius=0.002,
+        length=0.01,
         color=(1, 0, 1),
     ),
 ]
@@ -41,7 +41,7 @@ print("Interpenetration")
 start = time()
 
 for _ in range(100):
-    print(f"interpenetration volume: {cylinders[1].interference_volume_with_other_cylinder(cylinders[0], n_points=2000)}")
+    print(f"interpenetration volume: {cylinders[1].interference_volume_with_other_cylinder(cylinders[0], n_points=1000)}")
 
 
 end = time()
