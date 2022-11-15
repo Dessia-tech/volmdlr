c7d7daee29066835cbdfb710c3f9235af55aa92c
unknown <joly@dessia.tech>
2022-11-15 14:21:54 -0500

    step binary

--- volmdlr/step.py
+++ volmdlr/step.py
@@ -5,7 +5,8 @@
 """
 
 import time
-from typing import BinaryIO, List
+from typing import List
+from dessia_common.files import BinaryFile
 
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -275,7 +276,7 @@ class Step(dc.DessiaObject):
         return self._graph
 
     @classmethod
-    def from_stream(cls, stream: BinaryIO = None):
+    def from_stream(cls, stream: BinaryFile = None):
         stream.seek(0)
         lines = []
         for line in stream:
