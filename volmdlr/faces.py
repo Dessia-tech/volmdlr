34fa2c08ca672f8f901e1ede2530e17b25d413f5
Soumiya Bendjebla <bendjebla-soumiya@outlook.fr>
2022-11-21 15:28:23 +0100

    OpenShell3D: project_coincident_faces_of (update)

--- volmdlr/faces.py
+++ volmdlr/faces.py
@@ -7363,14 +7363,14 @@ class OpenShell3D(volmdlr.core.CompositePrimitive3D):
                 inside = set()
                 for c1 in c_inners_1:
                     for c2 in c_inners_2:
+                        # inside.add(c2.is_inside(c1))
                         if c1.is_superposing(c2):
                             inside.add(False)
                         else:
                             inside.add(c2.is_inside(c1))
-                            if c2.is_inside(c1):
-                                ax=c1.plot()
-                                c2.plot(ax, "r")
-                        # inside.add(c2.is_inside(c1))
+                            # if c2.is_inside(c1):
+                            #     ax=c1.plot()
+                            #     c2.plot(ax, "r")
 
                 if (face1.surface3d.is_coincident(face2.surface3d)
                     and (contour1.is_overlapping(contour2)
