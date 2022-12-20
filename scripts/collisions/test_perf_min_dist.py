e35d93b3b0ebf49e47d4773d41c0de0b3a1d1e11
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2022-12-20 18:54:33 +0100

    update test scripts

--- scripts/collisions/test_perf_min_dist.py
+++ scripts/collisions/test_perf_min_dist.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 from volmdlr.primitives3d import Cylinder
@@ -11,8 +12,8 @@ cylinder1 = Cylinder(
 )
 
 cylinder2 = Cylinder(
-    position=vm.Point3D(1, 0.11, 0),
-    axis=vm.Vector3D(1, 1, 0),
+    position=vm.Point3D(1, 0.11, 0.3),
+    axis=vm.Vector3D(1, 1, -6),
     radius=0.01,
     length=0.1,
 )
@@ -21,7 +22,7 @@ N = 100
 
 start = time.perf_counter()
 for _ in range(N):
-    dist = cylinder2.min_distance_to_other_cylinder(cylinder1)
-    # print(cylinder2.min_distance_to_other_cylinder(cylinder1))
+    # dist = cylinder2.min_distance_to_other_cylinder(cylinder1)
+    print(cylinder2.min_distance_to_other_cylinder(cylinder1))
 
 print(f"time per operation: {((time.perf_counter() - start)/N)*1000}ms")
