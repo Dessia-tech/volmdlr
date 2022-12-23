b4b6a968d1319dd11f202b64c824f5c80c2d2686
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2022-12-23 15:53:04 +0100

    style: black

--- tests/core_compiled/test_frame3d.py
+++ tests/core_compiled/test_frame3d.py
@@ -5,7 +5,9 @@ import volmdlr
 class TestFrame3D(unittest.TestCase):
     local_frame = volmdlr.Frame3D(
         volmdlr.Point3D(0.1, 0.3, 0.6),
-        volmdlr.Y3D, volmdlr.Z3D, volmdlr.X3D,
+        volmdlr.Y3D,
+        volmdlr.Z3D,
+        volmdlr.X3D,
     )
 
     def test_global_to_local_coordinates(self):
@@ -13,12 +15,12 @@ class TestFrame3D(unittest.TestCase):
         vector_local = self.local_frame.global_to_local_coordinates(vector_global)
 
         # Check that the converted vector has the expected coordinates
-        self.assertEqual(vector_local.x, 4-0.3)
-        self.assertEqual(vector_local.y, 5-0.6)
-        self.assertEqual(vector_local.z, 3-0.1)
+        self.assertEqual(vector_local.x, 4 - 0.3)
+        self.assertEqual(vector_local.y, 5 - 0.6)
+        self.assertEqual(vector_local.z, 3 - 0.1)
 
     def test_local_to_global_coordinates(self):
-        vector_local = volmdlr.Vector3D(4-0.3, 5-0.6, 3-0.1)
+        vector_local = volmdlr.Vector3D(4 - 0.3, 5 - 0.6, 3 - 0.1)
         vector_global = self.local_frame.local_to_global_coordinates(vector_local)
 
         # Check that the converted vector has the expected coordinates
