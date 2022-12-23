64383e79ea231fee0163aadc4e76860a19675f8f
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2022-12-23 11:48:40 +0100

    add new method with new names

--- volmdlr/core_compiled.pyx
+++ volmdlr/core_compiled.pyx
@@ -2382,7 +2382,31 @@ class Basis2D(Basis):
         matrix = self.inverse_transfer_matrix()
         return matrix.vector_multiplication(vector)
 
+    def global_to_local_coordinates(self, vector: Vector2D):
+        """
+        Convert the given vector's coordinates from the global landmark to the local landmark of this Basis2D.
+
+        :param vector: The vector to convert, given in global coordinates.
+        :type vector: :class:`volmdlr.Vector2D`
+        :return: The converted vector, in local coordinates.
+        :rtype: :class:`volmdlr.Vector2D`
+        """
+        matrix = self.inverse_transfer_matrix()
+        return matrix.vector_multiplication(vector)
+
     def old_coordinates(self, vector: Vector2D):
+        """
+        Convert the given vector's coordinates from the local landmark of this Basis2D to the global landmark.
+
+        :param vector: The vector to convert, given in local coordinates.
+        :type vector: :class:`volmdlr.Vector2D`
+        :return: The converted vector, in global coordinates.
+        :rtype: :class:`volmdlr.Vector2D`
+        """
+        matrix = self.transfer_matrix()
+        return matrix.vector_multiplication(vector)
+
+    def local_to_global_coordinates(self, vector: Vector2D):
         """
         Convert the given vector's coordinates from the local landmark of this Basis2D to the global landmark.
 
