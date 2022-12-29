460286bd69a9045d5f18669b7dd277c5148b4116
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2022-12-29 11:06:15 +0100

    Add a derivative method to BSplineCurve

--- volmdlr/edges.py
+++ volmdlr/edges.py
@@ -832,6 +832,33 @@ class BSplineCurve(Edge):
         point_dimension = f'Point{self.__class__.__name__[-2::]}'
         return [getattr(volmdlr, point_dimension)(*p) for p in curve_points]
 
+    def derivatives(self, u, order):
+        """ Evaluates n-th order curve derivatives at the given parameter value.
+
+        The output of this method is list of n-th order derivatives. If ``order`` is ``0``, then it will only output
+        the evaluated point. Similarly, if ``order`` is ``2``, then it will output the evaluated point, 1st derivative
+        and the 2nd derivative. For instance;
+
+        Example:
+
+        Assuming a curve (crv) is defined on a parametric domain [0.0, 1.0]
+        Let's take the curve derivative at the parametric position u = 0.35
+        ders = crv.derivatives(u=0.35, order=2).
+        ders[0]  # evaluated point, equal to crv.evaluate_single(0.35)
+        ders[1]  # 1st derivative at u = 0.35
+        ders[2]  @ 2nd derivative at u = 0.35
+
+        :param u: parameter value
+        :type u: float
+        :param order: derivative order
+        :type order: int
+        :return: a list containing up to {order}-th derivative of the curve
+        :rtype: Union[List[`volmdlr.Vector2D`], List[`volmdlr.Vector3D`]]
+        """
+
+        return [getattr(volmdlr, f'Vector{self.__class__.__name__[-2::]}')(*p)
+                for p in self.curve.derivatives(u, order)]
+
 
 class Line2D(Line):
     """
