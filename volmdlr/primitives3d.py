eaa5504f44b6bd6f6e09924500d8b91d1598f9a8
Soumiya Bendjebla <bendjebla-soumiya@outlook.fr>
2023-01-16 16:48:21 +0100

    Fix some D403 errors

--- volmdlr/primitives3d.py
+++ volmdlr/primitives3d.py
@@ -1601,7 +1601,7 @@ class HollowCylinder(RevolvedProfile):
 
     def translation(self, offset: volmdlr.Vector3D):
         """
-        HollowCylinder translation.
+        Translation for HollowCylinder.
 
         :param offset: translation vector.
         :return: A new translated HollowCylinder.
@@ -1613,7 +1613,7 @@ class HollowCylinder(RevolvedProfile):
 
     def translation_inplace(self, offset: volmdlr.Vector3D):
         """
-        HollowCylinder translation. Object is updated inplace.
+        Translation for HollowCylinder. Object is updated inplace.
 
         :param offset: translation vector.
         """
