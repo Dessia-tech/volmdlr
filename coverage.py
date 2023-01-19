8fd3e27c33be0f04ef91ca52f23c0ac31fc71350
Soumiya Bendjebla <bendjebla-soumiya@outlook.fr>
2023-01-19 09:58:51 +0100

    update coverage.py

--- coverage.py
+++ coverage.py
@@ -8,7 +8,7 @@ Created on Wed Sep 29 14:35:47 2021
 
 import json
 
-MIN_FILE_COVERAGE = 58.11
+MIN_FILE_COVERAGE = 55.12
 MIN_PROJECT_COVERAGE = 62.0
 
 untracked_modules = ['volmdlr/templates.py',
