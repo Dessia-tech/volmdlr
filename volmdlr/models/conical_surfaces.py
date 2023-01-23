d42caac4460e38329d43a65bf6c9bf797b8f9e5e
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-01-23 15:25:57 +0100

    small fix

--- volmdlr/models/conical_surfaces.py
+++ volmdlr/models/conical_surfaces.py
@@ -12,5 +12,4 @@ frame_cone = volmdlr.Frame3D(volmdlr.Point3D(0.0, 0.0, 0.1), volmdlr.X3D, -volmd
 conical_surface2 = vmf.ConicalSurface3D(frame_cone, 0.336674819387)
 
 frame = volmdlr.Frame3D(volmdlr.Point3D(0.0, 0.0, -0.0022143719324716786), volmdlr.X3D, volmdlr.Y3D, volmdlr.Z3D)
-semi_angle = 1.0471975511966
-conical_surface3 = vmf.ConicalSurface3D(frame, semi_angle)
+conical_surface3 = vmf.ConicalSurface3D(frame, 1.0471975511966)
