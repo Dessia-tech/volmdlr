b3ba418c7da9be2a165cd4a64ea92f76ca6d350e
WirajanDASILVA <wirajan.klebson@gmail.com>
2023-02-07 08:57:45 -0300

    fix some bugs

--- code_pep8.sh
+++ code_pep8.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # check pep8 formatting for changed files
 
-PEP8_CMD_TO_RUN='python3 -m autopep8 -i $(git diff --cached --name-only --diff-filter MARC | grep -E \.py$)'
+PEP8_CMD_TO_RUN="python3 -m autopep8 -i $(git diff --cached --name-only --diff-filter MARC | grep -E \.py$)"
 
 CHANGED_FILES=$(git diff --cached --name-only --diff-filter MARC | grep -E \.py$)
 if [[ -z "$CHANGED_FILES" ]]
   then exit 0
 fi
 
-DETECTED_CHANGES=$(python3 -m autopep8 -d $CHANGED_FILES)
+DETECTED_CHANGES=$(python3 -m autopep8 -d "$CHANGED_FILES")
 if [[ -n "$DETECTED_CHANGES" ]]
   then
   echo -e "\npep8 non conforming changes detected, please run :\n"
