58cef184fe1d1c24ab99f5823f7bf24ee5801493
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2023-02-13 12:04:50 +0100

    unittest: step_ids_to_str

--- tests/core/test_functions.py
+++ tests/core/test_functions.py
@@ -40,5 +40,16 @@ class TestDeleteDoublePoint(unittest.TestCase):
         self.assertTrue(volmdlr.Point3D(2, 2, 2) in result)
 
 
+class TestStepIdsToStr(unittest.TestCase):
+    ids_0 = [0]
+    ids_1 = [1, 2, 3, 4, 5]
+    ids_2 = [11, 22, 33, 44, 55]
+
+    def test_step_ids_to_str(self):
+        self.assertEqual(step_ids_to_str(self.ids_0), "#0")
+        self.assertEqual(step_ids_to_str(self.ids_1), "#1,#2,#3,#4,#5")
+        self.assertEqual(step_ids_to_str(self.ids_2), "#11,#22,#33,#44,#55")
+
+
 if __name__ == "__main__":
     unittest.main()
