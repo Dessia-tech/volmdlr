4b14ddc0e262acd6e297b3f1f2eb8987bb423593
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2023-02-13 12:42:31 +0100

    unittest: CompositePrimitive2D.rotation

--- tests/core/test_compositeprimitive2d.py
+++ tests/core/test_compositeprimitive2d.py
@@ -27,6 +27,14 @@ class TestCompositePrimitive2D(unittest.TestCase):
         self.assertEqual(self.composite_2d.primitives, self.primitives)
         self.assertEqual(self.composite_2d.name, "test")
 
+    def test_rotation(self):
+        center = volmdlr.Point2D(1.2, -3.4)
+        angle = 2.56
+        rotated_composite_2d = self.composite_2d.rotation(center, angle)
+
+        for p1, p2 in zip(rotated_composite_2d.primitives, self.primitives):
+            self.assertEqual(p1, p2.rotation(center, angle))
+
 
 if __name__ == "__main__":
     unittest.main()
