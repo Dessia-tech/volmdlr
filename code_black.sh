b9bc3910fea81c975a882a560b1a95641eb3b314
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2023-02-16 00:23:53 +0100

    fix black ci

--- code_black.sh
+++ code_black.sh
@@ -2,10 +2,12 @@
 # check black formatting on all files
 
 if black --check -l120 .; then
-    echo "\nBlack found no formatting errors."
+  echo
+  echo "Black found no formatting errors."
 else
-    echo "\nBlack found formatting errors."
-    echo "Please run 'black -l120 .' in source directory to format the code."
-    echo "You can run 'black --check --diff --color -l120' to see the difference with expected formatting."
-    exit 1
+  echo
+  echo "Black found formatting errors."
+  echo "Please run 'black -l120 .' in source directory to format the code."
+  echo "You can run 'black --check --diff --color -l120' to see the difference with expected formatting."
+  exit 1
 fi
