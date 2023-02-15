1e8f6d164abf2f91985fd57b6467a4d57e69a8cf
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2023-02-15 18:24:52 +0100

    remove unittests of inplace methods

--- tests/core/test_volume_model.py
+++ tests/core/test_volume_model.py
@@ -33,17 +33,6 @@ class TestVolumeModel(unittest.TestCase):
             p2 = p2.rotation(center, axis, angle)
             self.assertEqual(p1, p2)
 
-    def test_rotation_inplace(self):
-        center = volmdlr.Point3D(0.0, 0.0, 0.0)
-        axis = volmdlr.Vector3D(0.0, 0.0, 1.0)
-        angle = math.pi / 4
-        self.volume_model.rotation_inplace(center, axis, angle)
-
-        for p1, p2 in zip(self.volume_model.primitives, self.primitives):
-            self.assertNotEqual(p1, p2)
-            p2.rotation_inplace(center, axis, angle)
-            self.assertEqual(p1, p2)
-
     def test_translation(self):
         offset = volmdlr.Vector3D(1.0, 1.0, 1.0)
         translated_volume_model = self.volume_model.translation(offset)
@@ -51,15 +40,6 @@ class TestVolumeModel(unittest.TestCase):
         for p1, p2 in zip(translated_volume_model.primitives, self.primitives):
             self.assertNotEqual(p1, p2)
             p2 = p2.translation(offset)
-            self.assertEqual(p1, p2)
-
-    def test_translation_inplace(self):
-        offset = volmdlr.Vector3D(1.0, 1.0, 1.0)
-        self.volume_model.translation_inplace(offset)
-
-        for p1, p2 in zip(self.volume_model.primitives, self.primitives):
-            self.assertNotEqual(p1, p2)
-            p2.translation_inplace(offset)
             self.assertEqual(p1, p2)
 
     def test_frame_mapping(self):
@@ -70,16 +50,6 @@ class TestVolumeModel(unittest.TestCase):
         for p1, p2 in zip(mapped_volume_model.primitives, self.primitives):
             self.assertNotEqual(p1, p2)
             p2 = p2.frame_mapping(frame, side)
-            self.assertEqual(p1, p2)
-
-    def test_frame_mapping_inplace(self):
-        frame = volmdlr.Frame3D.from_point_and_vector(volmdlr.Point3D(1.0, 1.0, 1.0), volmdlr.Vector3D(1.0, 1.0, 1.0))
-        side = "old"
-        self.volume_model.frame_mapping_inplace(frame, side)
-
-        for p1, p2 in zip(self.volume_model.primitives, self.primitives):
-            self.assertNotEqual(p1, p2)
-            p2.frame_mapping_inplace(frame, side)
             self.assertEqual(p1, p2)
 
     def test_bounding_box(self):
