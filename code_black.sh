edbe951cb4d63824ccef69049d569605cff26ee1
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2023-02-16 01:20:29 +0100

    adjust drone message

--- code_black.sh
+++ code_black.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 # check black formatting on all files
 
-if black --check -l120 .; then
+if black --check .; then
   echo ""
   echo "Black found no formatting errors."
 else
   echo ""
   echo "Black found formatting errors."
-  echo "Please run 'black -l120 .' in source directory to format the code."
-  echo "You can run 'black --check --diff --color -l120' to see the difference with expected formatting."
+  echo "Please run 'black .' in source directory to format the code."
+  echo "You can run 'black --check --diff --color .' to see the difference with expected formatting."
+  echo ""
   echo "Consider upgrading black to latest version if you're still getting an error after formatting the code."
+  echo ""
+  echo "Note: Black is configurated in 'pyproject.toml' to have a 120 character line length."
+  echo "It is equivalent than using the 'l120' argument: 'black -l120 .'"
   exit 1
 fi
