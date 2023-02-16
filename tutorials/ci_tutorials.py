27b77c1649d4ccae547ea30a9f74b7f272d31d77
Pierre Gibertini <pierre.gibertini@etu.utc.fr>
2023-02-16 01:22:01 +0100

    style: black

--- tutorials/ci_tutorials.py
+++ tutorials/ci_tutorials.py
@@ -16,11 +16,17 @@ def convert_notebook(notebook_path, module_path):
     exporter.exclude_output_prompt = True
     source, meta = exporter.from_notebook_node(nb)
     lines = source.splitlines()
-    with open(module_path, 'w+') as fh:
+    with open(module_path, "w+") as fh:
         for line in lines:
             # Removing the prompt commands from notebook
-            if line[:20] != 'get_ipython().system' and '.plot(' not in line and line[:1] != '!'  and line[:12] != 'from IPython' and line[:4] != 'HTML':
-                fh.write(line + '\n')
+            if (
+                line[:20] != "get_ipython().system"
+                and ".plot(" not in line
+                and line[:1] != "!"
+                and line[:12] != "from IPython"
+                and line[:4] != "HTML"
+            ):
+                fh.write(line + "\n")
 
 
 tutorials = []
@@ -30,41 +36,41 @@ for tutorial in glob.glob("*.ipynb"):
     tutorials.append(tutorial[:-6])
 
 for tutorial in tutorials:
-    convert_notebook(tutorial + '.ipynb', tutorial + '.py')
+    convert_notebook(tutorial + ".ipynb", tutorial + ".py")
 
 for i in range(len(tutorials)):
-    tutorials[i] += '.py'
+    tutorials[i] += ".py"
 
 # Testing if all tutorials exists before launching them
 for tutorial_name in tutorials:
     if not os.path.isfile(tutorial_name):
-        raise FileNotFoundError(f'Script {tutorial_name} does not exists in CI tutorials')
+        raise FileNotFoundError(f"Script {tutorial_name} does not exists in CI tutorials")
 
 # Executing tutorials
-print('Executing tutorials for CI:')
+print("Executing tutorials for CI:")
 total_time = time.time()
 top_level_dir = os.getcwd()
 times = {}
 for tutorial_name in tutorials:
-    print(f'\t* {tutorial_name}')
+    print(f"\t* {tutorial_name}")
     # Reset dir
     os.chdir(top_level_dir)
     # Change cwd
-    if '/' in tutorial_name:
-        tutorial_folder = '/'.join(tutorial_name.split('/')[:-1])
+    if "/" in tutorial_name:
+        tutorial_folder = "/".join(tutorial_name.split("/")[:-1])
         if tutorial_folder:
             tutorial_folder = os.path.join(top_level_dir, tutorial_folder)
             os.chdir(tutorial_folder)
-    file_name = tutorial_name.split('/')[-1]
+    file_name = tutorial_name.split("/")[-1]
     t = time.time()
-    with open(file_name, 'r', encoding='utf-8') as tutorial:
+    with open(file_name, "r", encoding="utf-8") as tutorial:
         exec(tutorial.read())
     t = time.time() - t
     times[tutorial_name] = t
 
-print('Computation times:')
+print("Computation times:")
 for tutorial_name, t in sorted(times.items(), key=lambda x: x[1]):
-    print(f'* tutorial {tutorial_name}: {round(t, 3)} seconds ')
-    
+    print(f"* tutorial {tutorial_name}: {round(t, 3)} seconds ")
+
 total_time = time.time() - total_time
-print(f'Total time for CI tutorials: {total_time}')
+print(f"Total time for CI tutorials: {total_time}")
