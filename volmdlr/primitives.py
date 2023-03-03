07cc6c4c306dd27dfbbc1323576a2de1ac45a8fb
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-03-03 15:04:38 +0100

    fix point comparation from primitives.py

--- volmdlr/primitives.py
+++ volmdlr/primitives.py
@@ -196,10 +196,10 @@ class RoundedLineSegments:
                 primitives.append(arc1)
                 if iline + 1 in self.radius:
                     arc2 = arcs[iline + 1]
-                    if arc1.end.is_close(arc2.start):
+                    if not arc1.end.is_close(arc2.start):
                         primitives.append(self.line_class(arc1.end, arc2.start))
                 else:
-                    if arc1.end.is_close(self.points[iline + 1]):
+                    if not arc1.end.is_close(self.points[iline + 1]):
                         primitives.append(self.line_class(arc1.end, self.points[iline + 1]))
             else:
                 p1 = self.points[iline]
