c71707f1866ebad4116b86ac42ffa9d08f77c8a1
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-03-09 22:59:29 +0100

    small fix

--- volmdlr/step.py
+++ volmdlr/step.py
@@ -600,7 +600,6 @@ class Step(dc.DessiaObject):
     _standalone_in_db = True
 
     def __init__(self, lines: List[str], name: str = ''):
-        self.lines = lines
         self.functions, self.all_connections, self.connections = self.read_lines(lines)
         self._graph = None
         self.global_uncertainty = 1e-6
