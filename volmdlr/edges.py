790a36dc0d3c63c62ca291e300edb6a2092acc45
WirajanDASILVA <77811165+WirajanDASILVA@users.noreply.github.com>
2023-03-14 19:46:22 -0300

    Update volmdlr/edges.py
    Co-authored-by: GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>

--- volmdlr/edges.py
+++ volmdlr/edges.py
@@ -1609,7 +1609,7 @@ class BSplineCurve2D(BSplineCurve):
 
     def to_wire(self, n: int):
         """
-        Convert a Bspline curve to a wire 2d defined with 'n' line_segments.
+        Convert a Bspline curve to a wire 2D defined with 'n' line_segments.
 
         """
 
