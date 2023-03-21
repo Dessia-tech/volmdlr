76aed45f429155504d2ae7b322794203c12c525c
WirajanDASILVA <wirajan.klebson@gmail.com>
2023-03-21 10:55:50 -0300

    add some pylint fixes

--- volmdlr/edges.py
+++ volmdlr/edges.py
@@ -818,7 +818,7 @@ class BSplineCurve(Edge):
             x_init.append(xi)
 
         for x0 in x_init:
-            z = least_squares(f, x0=x0, bounds=([0, 1]))
+            z = least_squares(fun, x0=x0, bounds=([0, 1]))
             if z.fun < abs_tol:
                 return True
         return False
@@ -1326,9 +1326,11 @@ class Line2D(Line):
         # point will be called I(x_I, y_I)
         # self will be (AB)
         # line will be (CD)
-        i, a, b, c, d = self._compute_data_create_tangent_circle(self, point, other_line)
+        vector_i, vector_a, vector_b, vector_c, vector_d = self._compute_data_create_tangent_circle(
+            self, point, other_line)
         # Basis change
-        new_basis, new_a, new_b, new_c, new_d = self._change_reference_frame(i, a, b, c, d)
+        new_basis, new_a, new_b, new_c, new_d = self._change_reference_frame(vector_i, vector_a,
+                                                                             vector_b, vector_c, vector_d)
 
         if new_c[1] == 0 and new_d[1] == 0:
             # Segments are on the same line: no solution
@@ -5490,25 +5492,13 @@ class Arc3D(Arc):
         return point1, point2
 
     def distance_squared(self, x, u, v, k, w):
-        a = u.dot(u)
-        b = u.dot(v)
-        c = u.dot(k)
-        d = v.dot(v)
-        e = v.dot(k)
-        f = k.dot(k)
-        g = w.dot(u)
-        h = w.dot(v)
-        i = w.dot(k)
-        j = w.dot(w)
-
-        r = self.radius
-        return (a * x[0] ** 2 + j + d * (
-                (math.sin(x[1])) ** 2) * r ** 2 + f * (
-                        (math.cos(x[1])) ** 2) * r ** 2
-                - 2 * x[0] * g - 2 * x[0] * r * math.sin(x[1]) * b - 2 * x[
-                    0] * r * math.cos(x[1]) * c
-                + 2 * r * math.sin(x[1]) * h + 2 * r * math.cos(x[1]) * i
-                + math.sin(2 * x[1]) * e * r ** 2)
+        radius = self.radius
+        return (u.dot(u) * x[0] ** 2 + w.dot(w) + v.dot(v) * (
+                (math.sin(x[1])) ** 2) * radius ** 2 + k.dot(k) * ((math.cos(x[1])) ** 2) * radius ** 2
+                - 2 * x[0] * w.dot(u) - 2 * x[0] * radius * math.sin(x[1]) * u.dot(v) - 2 * x[
+                    0] * radius * math.cos(x[1]) * u.dot(k)
+                + 2 * radius * math.sin(x[1]) * w.dot(v) + 2 * radius * math.cos(x[1]) * w.dot(k)
+                + math.sin(2 * x[1]) * v.dot(k) * radius ** 2)
 
     def minimum_distance_points_line(self, other_line):
         u = other_line.direction_vector()
