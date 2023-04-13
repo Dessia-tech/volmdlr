096788a375ec92783594ee7ceb59a0a90ccad279
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-04-13 14:50:08 +0200

    remove unused import

--- volmdlr/edges.py
+++ volmdlr/edges.py
@@ -16,7 +16,7 @@ import matplotlib.pyplot as plt
 import numpy as npy
 import plot_data.core as plot_data
 import scipy.integrate as scipy_integrate
-from scipy.optimize import least_squares, minimize
+from scipy.optimize import least_squares
 from geomdl import NURBS, BSpline, fitting, operations, utilities
 from geomdl.operations import length_curve, split_curve
 from matplotlib import __version__ as _mpl_version
