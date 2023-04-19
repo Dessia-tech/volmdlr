f307fb5ac923a40fcfbea777d329fa30cf190220
Soumiya Bendjebla <bendjebla-soumiya@outlook.fr>
2023-04-19 12:30:53 +0200

    GmshParser: to_vtk (consider quadratic tetrahedron element)

--- volmdlr/gmsh_vm.py
+++ volmdlr/gmsh_vm.py
@@ -729,7 +729,10 @@ class GmshParser(DessiaObject):
 
         cells_str_int = {'elements_type_1': ('2 ', 3),
                          'elements_type_2': ('3 ', 4),
-                         'elements_type_4': ('4 ', 5)}
+                         'elements_type_4': ('4 ', 5),
+                         'elements_type_8': ('3 ', 4),
+                         'elements_type_9': ('6 ', 7),
+                         'elements_type_11': ('10 ', 11)}
 
         for key, value in cells_str_int.items():
             cells = 0
@@ -738,6 +741,7 @@ class GmshParser(DessiaObject):
                     for element in map(str, elements):
                         lines.append(value[0] + element[1:-1].replace(',', ''))
                         cells += 1
+
                 cells_1 += cells * value[1]
                 cells_0 += cells
             except KeyError:
@@ -758,7 +762,10 @@ class GmshParser(DessiaObject):
 
         cells_str_int = {'elements_type_1': '3',
                          'elements_type_2': '5',
-                         'elements_type_4': '10'}
+                         'elements_type_4': '10',
+                         'elements_type_8': '21',
+                         'elements_type_9': '22',
+                         'elements_type_11': '24'}
 
         for key, value in cells_str_int.items():
             try:
