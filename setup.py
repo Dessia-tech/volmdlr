5947880a454bf0c481a2fe0a615d6f9d692538e5
WirajanDASILVA <77811165+WirajanDASILVA@users.noreply.github.com>
2023-06-08 14:18:36 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -150,5 +150,5 @@ setup(
                            "volmdlr/bspline_compiled.pyx",
                            "volmdlr/bspline_fitting.pyx",
                            "volmdlr/bspline_evaluators.pyx"]),
-    python_requires=">=3.7",
+    python_requires=">=3.9",
 )
