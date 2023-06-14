84f99c39a4906c4c250115e7b3237fbc76608c16
Soumiya Bendjebla <bendjebla-soumiya@outlook.fr>
2023-06-14 10:38:36 +0200

    ClosedShell3D: subtract_to_closed_shell (use from_faces)

--- volmdlr/shells.py
+++ volmdlr/shells.py
@@ -1539,9 +1539,9 @@ class ClosedShell3D(OpenShell3D):
         faces += shell2.get_non_intersecting_faces(self, intersecting_faces, intersection_method=True)
         new_valid_faces = self.subtraction_faces(shell2, intersecting_faces, intersecting_combinations)
         faces += new_valid_faces
-        new_shell = ClosedShell3D(faces)
+        # new_shell = ClosedShell3D(faces)
         # new_shell.eliminate_not_valid_closedshell_faces()
-        return [new_shell]
+        return self.from_faces(faces)
 
     def validate_intersection_operation(self, shell2):
         """
