34281ccca68cce16fde18317fb03dcedfa5d88fa
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-06-15 14:20:11 +0200

    Add name in script test

--- scripts/primitives/sphere.py
+++ scripts/primitives/sphere.py
@@ -4,5 +4,5 @@ Demo script of creating and displaying a Sphere.
 import volmdlr
 from volmdlr.primitives3d import Sphere
 
-sphere = Sphere(volmdlr.O3D, 0.1)
+sphere = Sphere(volmdlr.O3D, 0.1, name="Sphere")
 sphere.babylonjs()
