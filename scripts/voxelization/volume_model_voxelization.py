e09429b98bf4643b47d263bf0fe3edd182e99057
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-06-16 18:41:37 +0200

    Some tesing

--- scripts/voxelization/volume_model_voxelization.py
+++ scripts/voxelization/volume_model_voxelization.py
@@ -12,8 +12,12 @@ cylinder = Cylinder(volmdlr.Point3D(0.0, 0.0, 0.1), volmdlr.Z3D, 0.1, 0.2, name=
 volume_model = VolumeModel([sphere, cylinder])
 
 # Voxelize the volume model (it uses the triangulated model to create the voxelization)
-voxelization = Voxelization.from_volume_model(volume_model, 0.1, name="Voxels")
+voxelization = Voxelization.from_volume_model(volume_model, 0.005, name="Voxels")
 
 # Display the result
 volume_model.primitives.extend(voxelization.volmdlr_primitives())
 volume_model.babylonjs()
+
+# Compare triangulation methods
+voxelization.to_closed_shell().babylonjs()
+voxelization.to_closed_triangle_shell().babylonjs()
