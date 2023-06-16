c83c83bde19d432b2969c2c6806632fa06567ca5
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-06-16 11:53:34 +0200

    More interesting test script

--- scripts/voxelization/volume_model_voxelization.py
+++ scripts/voxelization/volume_model_voxelization.py
@@ -2,16 +2,17 @@
 Voxelization of a closed shell.
 """
 from volmdlr.voxelization import Voxelization
-from volmdlr.primitives3d import Sphere
+from volmdlr.primitives3d import Sphere, Cylinder
 from volmdlr.core import VolumeModel
 import volmdlr
 
 # Create a volume model
 sphere = Sphere(volmdlr.O3D, 0.1, name="Sphere")
-volume_model = VolumeModel([sphere])
+cylinder = Cylinder(volmdlr.Point3D(0.0, 0.0, 0.1), volmdlr.Z3D, 0.1, 0.2, name="Cylinder")
+volume_model = VolumeModel([sphere, cylinder])
 
 # Voxelize the volume model (it uses the triangulated model to create the voxelization)
-voxelization = Voxelization.from_volume_model(volume_model, 0.01)
+voxelization = Voxelization.from_volume_model(volume_model, 0.005)
 
 # Display the result
 volume_model.primitives.extend(voxelization.volmdlr_primitives())
