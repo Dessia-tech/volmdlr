11deaa2c02706c28f22e256cad8413c7d8159b57
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-06-26 16:48:24 +0200

    fix ununsed arg

--- volmdlr/core.py
+++ volmdlr/core.py
@@ -409,7 +409,7 @@ class Primitive3D(dc.PhysicalObject):
         raise NotImplementedError(
             f"triangulation method should be implemented on class {self.__class__.__name__}")
 
-    def babylon_meshes(self, merge_meshes=True):
+    def babylon_meshes(self, *args, **kwargs):
         """
         Returns the babylonjs mesh.
         """
