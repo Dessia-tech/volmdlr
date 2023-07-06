08566c2e1aff2402c0ea50e8754d7fbb86d7d66e
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-07-06 15:31:22 +0200

    fix shape representation behavior

--- volmdlr/step.py
+++ volmdlr/step.py
@@ -451,18 +451,22 @@ def manifold_surface_shape_representation(arguments, object_dict):
     Returns the data in case of a manifold_surface_shape_representation, interpreted as shell3D.
     """
     primitives = []
+    shell_primitives = []
+    only_open_shell = True
     for arg in arguments[1]:
         primitive = object_dict[int(arg[1:])]
         if isinstance(primitive, vmshells.OpenShell3D):
             primitives.append(primitive)
+            shell_primitives.extend(primitive.primitives)
         if isinstance(primitive, volmdlr.core.Compound):
+            only_open_shell = False
             counter = 0
             for sub_prim in primitive.primitives:
                 sub_prim.name = arguments[0][1:-1] + str(counter)
                 counter += 1
             primitives.append(primitive)
-    if len(primitives) == 1:
-        return primitives[0]
+    if only_open_shell:
+        return vmshells.OpenShell3D(shell_primitives)
     return volmdlr.core.Compound(primitives)
 
 
@@ -564,18 +568,22 @@ def advanced_brep_shape_representation(arguments, object_dict):
 
     """
     primitives = []
+    shell_primitives = []
+    only_closed_shell = True
     for arg in arguments[1]:
         primitive = object_dict[int(arg[1:])]
         if isinstance(primitive, vmshells.OpenShell3D):
             primitives.append(primitive)
+            shell_primitives.extend(primitive.primitives)
         if isinstance(primitive, volmdlr.core.Compound):
+            only_closed_shell = False
             counter = 0
             for sub_prim in primitive.primitives:
                 sub_prim.name = arguments[0][1:-1] + str(counter)
                 counter += 1
             primitives.append(primitive)
-    if len(primitives) == 1:
-        return primitives[0]
+    if only_closed_shell:
+        return vmshells.ClosedShell3D(shell_primitives)
     return volmdlr.core.Compound(primitives)
 
 
