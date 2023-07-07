ff5a64eb4e54587e3c54058a20912fc9545b754a
WirajanDASILVA <wirajan.klebson@gmail.com>
2023-07-07 08:10:01 -0300

    fix arc3D line_intersections

--- tests/edges/test_fullarc3d.py
+++ tests/edges/test_fullarc3d.py
@@ -16,10 +16,10 @@ class TestFullArc3D(unittest.TestCase):
         line3d_ = edges.LineSegment3D(volmdlr.Point3D(-.15, 0, 0), volmdlr.Point3D(-0.15, 0.0, -.35))
         linesegment_intesections1 = self.fullarc3d.linesegment_intersections(line3d)
         self.assertEqual(len(linesegment_intesections1), 2)
-        self.assertEqual(linesegment_intesections1[0],
-                         volmdlr.Point3D(0.10606601717798213, 0.10606601717798214, -0.25))
-        self.assertEqual(linesegment_intesections1[1],
-                         volmdlr.Point3D(-0.10606601717798213, -0.10606601717798213, -0.25))
+        self.assertTrue(linesegment_intesections1[0].is_close(
+            volmdlr.Point3D(0.10606601717798213, 0.10606601717798214, -0.25)))
+        self.assertTrue(linesegment_intesections1[1].is_close(
+                         volmdlr.Point3D(-0.10606601717798213, -0.10606601717798213, -0.25)))
         linesegment_intesections2 = self.fullarc3d.linesegment_intersections(line3d_)
         self.assertEqual(len(linesegment_intesections2), 1)
         self.assertEqual(linesegment_intesections2[0], volmdlr.Point3D(-0.15, 0.0, -0.25))
