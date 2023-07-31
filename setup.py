0791097318dbe1ed24c3b7bc6df896c057d2b53a
WirajanDASILVA <wirajan.klebson@gmail.com>
2023-07-31 10:57:31 -0300

    set scypy<1.10.1 as requirement

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ setup(
         "Cython",
         "numpy<=1.24.0",
         "matplotlib<=3.4.3",  # remove when https://github.com/DanPorter/Dans_Diffraction/pull/11 available
-        "scipy",
+        "scipy<1.10.1",
         "geomdl",
         "networkx",
         "triangle",
