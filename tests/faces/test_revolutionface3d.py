915b4d8d31aa7250785a0e26da6e91095ab67eed
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-08-23 11:23:24 +0200

    fix test revolutionface

--- tests/faces/test_revolutionface3d.py
+++ tests/faces/test_revolutionface3d.py
@@ -8,9 +8,9 @@ from volmdlr.step import Step
 class TestRevolutionFace3D(unittest.TestCase):
 
     def test_to_step(self):
-        model = VolumeModel.load_from_file("objects_revolution_tests/revolutionface_export_test.json")
-        model.to_step("objects_revolution_tests/test_export.step")
-        step_import = Step.from_file("objects_revolution_tests/test_export.step")
+        model = VolumeModel.load_from_file("faces/objects_revolution_tests/revolutionface_export_test.json")
+        model.to_step("faces/objects_revolution_tests/test_export.step")
+        step_import = Step.from_file("faces/objects_revolution_tests/test_export.step")
         model2 = step_import.to_volume_model()
         revolutionface = model2.primitives[0].primitives[0]
         self.assertTrue(revolutionface.outer_contour3d.is_ordered())
