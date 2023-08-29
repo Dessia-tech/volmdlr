c13405ffd90810f5d39b39134402f24ebb2bbff0
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-08-29 16:45:36 +0200

    Update cad_simplification test scripts

--- scripts/cad_simplifier/octree_block_simplify.py
+++ scripts/cad_simplifier/octree_block_simplify.py
@@ -3,18 +3,24 @@ Showcase of the 'OctreBlockSimplify'
 """
 import time
 
-from volmdlr.shells import ClosedShell3D
 from volmdlr.cad_simplification import OctreeBlockSimplify
+from volmdlr.step import Step
 from volmdlr.core import VolumeModel
 
-closed_shell = ClosedShell3D.load_from_file("model.json")
+# Load
+volume_model = Step.from_file("engine.step").to_volume_model()
+closed_shell = volume_model.get_shells()[0]
 
+# Simplify
 start = time.perf_counter()
 octre_block_simplify = OctreeBlockSimplify(closed_shell=closed_shell)
 simplified_closed_shell = octre_block_simplify.simplify(depth=3)
 
 print(f"Simplification took {time.perf_counter() - start:.6f} seconds")
 
+# Display
+simplified_closed_shell.color = (1.0, 0.0, 0.0)
 simplified_closed_shell.alpha = 0.6
+
 volume_model = VolumeModel([closed_shell, simplified_closed_shell])
 volume_model.babylonjs()
---
+++ scripts/cad_simplifier/triple_extrusion_simplify.py
@@ -0,0 +1,25 @@
+"""
+Showcase of the 'OctreBlockSimplify'
+"""
+import time
+
+from volmdlr.cad_simplification import TripleExtrusionSimplify
+from volmdlr.step import Step
+
+# Load
+volume_model = Step.from_file("engine.step").to_volume_model()
+
+# Simplify
+start = time.perf_counter()
+octre_block_simplify = TripleExtrusionSimplify(volume_model=volume_model)
+simplified_volume_model = octre_block_simplify.simplify()
+
+print(f"Simplification took {time.perf_counter() - start:.6f} seconds")
+
+# Display
+simplified_closed_shell = simplified_volume_model.get_shells()[0]
+simplified_closed_shell.color = (1.0, 0.0, 0.0)
+simplified_closed_shell.alpha = 0.6
+
+volume_model.primitives.append(simplified_closed_shell)
+volume_model.babylonjs()
