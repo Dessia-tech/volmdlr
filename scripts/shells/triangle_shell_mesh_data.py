45df1c0f559efdeb473d6900b3e5696cdf913df0
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-08-30 15:45:42 +0200

    Update test script

--- scripts/shells/triangle_shell_mesh_data.py
+++ scripts/shells/triangle_shell_mesh_data.py
@@ -1,13 +1,16 @@
 """
-Showcase of TriangleShell3D to_mesh_data and from_mesh_data methods.
+Showcase of TriangleShell3D import / export.
 """
 
 from volmdlr.stl import Stl
 from volmdlr.shells import ClosedTriangleShell3D
 
-triangle_shell = Stl.load_from_file("../stl/simple.stl").to_closed_shell()
+triangle_shell_1 = Stl.load_from_file("../stl/simple.stl").to_closed_shell()
 
-mesh_data = triangle_shell.to_mesh_data()
+mesh_data = triangle_shell_1.to_mesh_data()
 triangle_shell_2 = ClosedTriangleShell3D.from_mesh_data(mesh_data[0], mesh_data[1])
 
-triangle_shell.save_to_file("triangle_shell_opti.json")
+triangle_shell_1.save_to_file("triangle_shell.json")
+triangle_shell_3 = ClosedTriangleShell3D.load_from_file("triangle_shell.json")
+
+print(triangle_shell_3 == triangle_shell_1)
