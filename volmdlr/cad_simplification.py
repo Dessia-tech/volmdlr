c6870ada7c19d248820660010fc70907f140345f
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-08-30 11:26:46 +0200

    Feat: TriangleDecimationSimplify

--- volmdlr/cad_simplification.py
+++ volmdlr/cad_simplification.py
@@ -3,6 +3,8 @@ volmdlr cad simplification module.
 """
 from abc import ABC
 
+import numpy as np
+import pyfqmr
 from dessia_common.core import DessiaObject
 
 import volmdlr
@@ -10,6 +12,7 @@ from volmdlr.cloud import PointCloud3D
 from volmdlr.core import VolumeModel
 from volmdlr.discrete_representation import MatrixBasedVoxelization
 from volmdlr.primitives3d import ExtrudedProfile
+from volmdlr.shells import ClosedTriangleShell3D
 from volmdlr.wires import Contour2D
 
 
@@ -115,3 +118,79 @@ class VoxelizationSimplify(Simplify):
             voxelization = voxelization.fill_enclosed_voxels()
 
         return VolumeModel(voxelization.volmdlr_primitives())
+
+
+class TriangleDecimationSimplify(Simplify):
+    """CAD simplification based on 'triangle decimation' method."""
+
+    def simplify(
+        self,
+        target_ratio: float,
+        update_rate: int = 5,
+        aggressiveness: float = 7.0,
+        max_iterations: int = 100,
+        verbose: bool = False,
+        lossless: bool = False,
+        threshold_lossless: float = 1e-3,
+        alpha: float = 1e-9,
+        k: int = 3,
+        preserve_border: bool = True,
+    ):
+        """
+        Simplify the VolumeModel using the 'triangle decimation' method, and return it.
+
+        To do so, it triangulates each shell of the given volume model and then decimates it at the given target ratio.
+
+        Note: threshold = alpha * pow(iteration + k, aggressiveness)
+
+        :param target_ratio: Target number of triangles. Not used if `lossless` is True.
+        :type target_ratio: float
+        :param update_rate: Number of iterations between each update. If `lossless` flag is set to True, rate is 1.
+        :type update_rate: int
+        :param aggressiveness: Parameter controlling the growth rate of the threshold at each iteration when `lossless` is False.
+        :type aggressiveness: float
+        :param max_iterations: Maximal number of iterations.
+        :type max_iterations: int
+        :param verbose: Control verbosity.
+        :type verbose: bool
+        :param lossless: Use the lossless simplification method.
+        :type lossless: bool
+        :param threshold_lossless: Maximal error after which a vertex is not deleted. Only for `lossless` method.
+        :type threshold_lossless: float
+        :param alpha: Parameter for controlling the threshold growth.
+        :type alpha: float
+        :param k: Parameter for controlling the threshold growth.
+        :type k: int
+        :param preserve_border: Flag for preserving vertices on open border.
+        :type preserve_border: bool
+
+        :return: The decimated VolumeModel.
+        :rtype: VolumeModel
+        """
+        decimated_shells = []
+        simplifier = pyfqmr.Simplify()
+
+        for shell in self.volume_model.get_shells():
+            triangulation = shell.triangulation()
+
+            vertices = np.array([(point.x, point.y, point.z) for point in triangulation.points])
+            triangles = np.array(triangulation.triangles)
+
+            simplifier.setMesh(vertices, triangles)
+            simplifier.simplify_mesh(
+                target_count=int(target_ratio * len(triangles)),
+                update_rate=update_rate,
+                aggressiveness=aggressiveness,
+                max_iterations=max_iterations,
+                verbose=verbose,
+                lossless=lossless,
+                threshold_lossless=threshold_lossless,
+                alpha=alpha,
+                K=k,
+                preserve_border=preserve_border,
+            )
+
+            vertices, faces, _ = simplifier.getMesh()
+            decimated_shells.append(ClosedTriangleShell3D.from_mesh_data(vertices, faces))
+
+        return VolumeModel(decimated_shells)
