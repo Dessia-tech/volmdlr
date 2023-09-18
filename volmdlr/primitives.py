d9cbe06caa381867bc2669cfa5d0f7737eddf5ec
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-09-18 12:18:37 +0200

    pylint_18_09_2023

--- volmdlr/primitives.py
+++ volmdlr/primitives.py
@@ -179,10 +179,10 @@ class RoundedLineSegments:
                             b_ub[ieq_ub] = lines_length[ip1]
                             ieq_ub += 1
 
-                    d_ = linprog(c, a_ub, b_ub, bounds=bounds)
+                    optimized_radius_solution = linprog(c, a_ub, b_ub, bounds=bounds)
 
                     for ipoint, dof_point in dof.items():
-                        radius = d_.x[dof_point] * math.tan(alpha[ipoint])
+                        radius = optimized_radius_solution .x[dof_point] * math.tan(alpha[ipoint])
                         if radius > 1e-10:
                             self.radius[ipoint] = radius
                         else:
