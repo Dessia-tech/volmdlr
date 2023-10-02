27807a14ec96b6f11bcc7cfb4d5ba297130f9db4
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-10-02 19:24:38 +0200

    fix test_planeface3d

--- tests/faces/test_planeface3d.py
+++ tests/faces/test_planeface3d.py
@@ -196,10 +196,11 @@ class TestPlaneFace3D(unittest.TestCase):
         plane4 = surfaces.Plane3D(frame)
         face = get_face(plane4)
         intersections = face.face_intersections(conical_face)
-        self.assertEqual(len(intersections), 1)
+        self.assertEqual(len(intersections), 2)
         self.assertIsInstance(intersections[0].primitives[0], edges.ArcEllipse3D)
-        self.assertAlmostEqual(intersections[0].length(), 3.0892620474828516)
-
+        self.assertIsInstance(intersections[1].primitives[0], edges.ArcEllipse3D)
+        self.assertAlmostEqual(intersections[0].length(), 1.1339664625179093)
+        self.assertAlmostEqual(intersections[1].length(), 0.7233023692399578)
         """================== Parabola ===================="""
         point1 = conical_surface.frame.origin
         point2 = conical_surface.frame.local_to_global_coordinates(
