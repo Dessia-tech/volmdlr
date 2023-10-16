f4c6c264380c2d6e9d9ec3712479c5808aa757da
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-10-16 17:44:12 +0200

    Test fix ci_scripts.py

--- scripts/cad_simplification/triple_extrusion_simplify.py
+++ scripts/cad_simplification/triple_extrusion_simplify.py
@@ -7,7 +7,7 @@ from volmdlr.cad_simplification import TripleExtrusionSimplify
 from volmdlr.step import Step
 
 # Load
-volume_model = Step.from_file("engine.step").to_volume_model()
+volume_model = Step.from_file("./engine.step").to_volume_model()
 
 # Simplify
 start = time.perf_counter()
