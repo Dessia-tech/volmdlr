f8debc30449a28b1a69392ed160c656f58b5c00c
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-10-18 11:08:14 +0200

    Style: black

--- volmdlr/edges.py
+++ volmdlr/edges.py
@@ -3172,8 +3172,11 @@ class Arc2D(ArcMixin, Edge):
         :param angle: angle rotation.
         :return: a new rotated Arc2D.
         """
-        return Arc2D(*[point.rotation(center, angle) if point else point for point in
-                       [self.circle, self.start, self.end]], is_trigo=self.is_trigo, name=self.name)
+        return Arc2D(
+            *[point.rotation(center, angle) if point else point for point in [self.circle, self.start, self.end]],
+            is_trigo=self.is_trigo,
+            name=self.name
+        )
 
     def translation(self, offset: volmdlr.Vector2D):
         """
@@ -3182,8 +3185,11 @@ class Arc2D(ArcMixin, Edge):
         :param offset: translation vector.
         :return: A new translated Arc2D.
         """
-        return Arc2D(*[point.translation(offset) if point else point for point in
-                       [self.circle, self.start, self.end]], is_trigo=self.is_trigo, name=self.name)
+        return Arc2D(
+            *[point.translation(offset) if point else point for point in [self.circle, self.start, self.end]],
+            is_trigo=self.is_trigo,
+            name=self.name
+        )
 
     def frame_mapping(self, frame: volmdlr.Frame2D, side: str):
         """
@@ -3191,8 +3197,13 @@ class Arc2D(ArcMixin, Edge):
 
         side = 'old' or 'new'
         """
-        return Arc2D(self.circle.frame_mapping(frame, side), self.start.frame_mapping(frame, side),
-                     self.end.frame_mapping(frame, side), is_trigo=self.is_trigo, name=self.name)
+        return Arc2D(
+            self.circle.frame_mapping(frame, side),
+            self.start.frame_mapping(frame, side),
+            self.end.frame_mapping(frame, side),
+            is_trigo=self.is_trigo,
+            name=self.name,
+        )
 
     def second_moment_area(self, point):
         """
