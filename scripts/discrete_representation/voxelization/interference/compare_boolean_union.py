f1579813c94c83a45d25fa89293394308137d17f
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-11-02 17:02:54 +0100

    Testing union

--- scripts/discrete_representation/voxelization/interference/compare_boolean_union.py
+++ scripts/discrete_representation/voxelization/interference/compare_boolean_union.py
@@ -32,22 +32,6 @@ volume_model_3.primitives[0].color = (0, 0, 1)
 volume_model = VolumeModel(volume_model_1.primitives + volume_model_2.primitives + volume_model_3.primitives)
 # volume_model.babylonjs()
 
-print("\nPointBasedVoxelization")
-t0 = time.perf_counter()
-voxelization_1 = PointBasedVoxelization.from_volume_model(volume_model_1, VOXEL_SIZE)
-voxelization_2 = PointBasedVoxelization.from_volume_model(volume_model_2, VOXEL_SIZE)
-voxelization_3 = PointBasedVoxelization.from_volume_model(volume_model_3, VOXEL_SIZE)
-t1 = time.perf_counter()
-union_11 = voxelization_1.union(voxelization_2)
-t2 = time.perf_counter()
-union_12 = voxelization_1.union(voxelization_3)
-t3 = time.perf_counter()
-union_13 = voxelization_2.union(voxelization_3)
-t4 = time.perf_counter()
-print(
-    f"Voxelization: {(t1-t0)*1000:.3f}ms, 1/2: {(t2-t1)*1000:.3f}ms, 1/3: {(t3-t2)*1000:.3f}ms, 2/3: {(t4-t3)*1000:.3f}ms"
-)
-
 print("\nOctreeBasedVoxelization")
 t0 = time.perf_counter()
 voxelization_1 = OctreeBasedVoxelization.from_volume_model(volume_model_1, VOXEL_SIZE)
@@ -59,6 +43,22 @@ t2 = time.perf_counter()
 union_22 = voxelization_1.union(voxelization_3)
 t3 = time.perf_counter()
 union_23 = voxelization_2.union(voxelization_3)
+t4 = time.perf_counter()
+print(
+    f"Voxelization: {(t1-t0)*1000:.3f}ms, 1/2: {(t2-t1)*1000:.3f}ms, 1/3: {(t3-t2)*1000:.3f}ms, 2/3: {(t4-t3)*1000:.3f}ms"
+)
+
+print("\nPointBasedVoxelization")
+t0 = time.perf_counter()
+voxelization_1 = voxelization_1.to_point_based_voxelization()
+voxelization_2 = voxelization_2.to_point_based_voxelization()
+voxelization_3 = voxelization_3.to_point_based_voxelization()
+t1 = time.perf_counter()
+union_11 = voxelization_1.union(voxelization_2)
+t2 = time.perf_counter()
+union_12 = voxelization_1.union(voxelization_3)
+t3 = time.perf_counter()
+union_13 = voxelization_2.union(voxelization_3)
 t4 = time.perf_counter()
 print(
     f"Voxelization: {(t1-t0)*1000:.3f}ms, 1/2: {(t2-t1)*1000:.3f}ms, 1/3: {(t3-t2)*1000:.3f}ms, 2/3: {(t4-t3)*1000:.3f}ms"
