e0b3e34942d75c3d2985b076da8aa358b35cb28a
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-11-08 18:18:07 +0100

    Script: bbox_triangle_intersection.py

---
+++ scripts/core/bbox_triangle_intersection.py
@@ -0,0 +1,19 @@
+"""
+Showcase of BoundingBox / Triangle3D intersection
+"""
+from volmdlr.core import BoundingBox
+from volmdlr.faces import Triangle3D
+import volmdlr
+
+bounding_box1 = BoundingBox(0, 1, 0, 1, 0, 1)
+bounding_box2 = BoundingBox(1, 2, 1, 2, 1, 2)
+
+p0 = volmdlr.Point3D(5, -3, 0.5)
+p1 = volmdlr.Point3D(-3, 5, 0.5)
+p2 = volmdlr.Point3D(5, 5, 0.5)
+triangle = Triangle3D(p0, p1, p2)
+
+ax = bounding_box1.plot()
+triangle.plot(ax=ax)
+
+print(bounding_box1.is_intersecting_triangle(triangle))
