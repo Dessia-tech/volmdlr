133fde335443bc0abd596ce63a6d6ae69a207cae
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-11-08 18:41:21 +0100

    Unittest: BoundingBox.is_intersecting_triangle

--- scripts/core/bbox_triangle_intersection.py
+++ scripts/core/bbox_triangle_intersection.py
@@ -5,15 +5,15 @@ from volmdlr.core import BoundingBox
 from volmdlr.faces import Triangle3D
 import volmdlr
 
-bounding_box1 = BoundingBox(0, 1, 0, 1, 0, 1)
-bounding_box2 = BoundingBox(1, 2, 1, 2, 1, 2)
+bounding_box = BoundingBox(0.0, 2.0, 0.0, 2.0, 0.0, 2.0)
 
 p0 = volmdlr.Point3D(5, -3, 0.5)
 p1 = volmdlr.Point3D(-3, 5, 0.5)
 p2 = volmdlr.Point3D(5, 5, 0.5)
 triangle = Triangle3D(p0, p1, p2)
 
-ax = bounding_box1.plot()
+ax = bounding_box.plot()
 triangle.plot(ax=ax)
 
-print(bounding_box1.is_intersecting_triangle(triangle))
+print(bounding_box.is_intersecting_triangle(triangle))
+Uni
