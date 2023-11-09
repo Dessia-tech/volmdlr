19ae3d094e12f90dc938a91cbbd58c597bc0e305
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-11-09 10:50:14 +0100

    increase too-many-locals seuil

--- code_pylint.py
+++ code_pylint.py
@@ -23,7 +23,7 @@ MAX_ERROR_BY_TYPE = {
                      'inconsistent-return-statements': 4,
                      'unused-variable': 22,
                      'arguments-differ': 64,
-                     'too-many-locals': 93,
+                     'too-many-locals': 94,
                      'unused-argument': 8,
                      'too-many-arguments': 26,
                      'line-too-long': 12,
