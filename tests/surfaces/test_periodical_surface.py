06cf3e1dfe309873df7954d7b43d2e01bdfd3ac8
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-11-14 11:09:22 +0100

    fix unittests

--- tests/surfaces/test_periodical_surface.py
+++ tests/surfaces/test_periodical_surface.py
@@ -9,9 +9,9 @@ from volmdlr import surfaces
 class TestPeriodicalSurface(unittest.TestCase):
 
     def test_bsplinecurve3d_to_2d(self):
-        surface = surfaces.CylindricalSurface3D.from_json(
+        surface = surfaces.CylindricalSurface3D.load_from_file(
             "surfaces/objects_periodical_surface/periodicalsurface_with_theta_discontinuity.json")
-        bspline = vme.BSplineCurve3D.from_json(
+        bspline = vme.BSplineCurve3D.load_from_file(
             "surfaces/objects_periodical_surface/bsplinecurve_with_theta_discontinuity.json")
         bspline2d = surface.bsplinecurve3d_to_2d(bspline)[0]
         theta1 = bspline2d.start.x
