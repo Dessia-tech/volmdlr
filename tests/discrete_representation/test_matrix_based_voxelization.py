0dabd2dbc4dfb77fae9db1083da367035d28bf0a
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-11-15 17:37:29 +0100

    Test: from display triangle shell

--- tests/discrete_representation/test_matrix_based_voxelization.py
+++ tests/discrete_representation/test_matrix_based_voxelization.py
@@ -72,9 +72,11 @@ class TestMatrixBasedVoxelizationCreation(unittest.TestCase):
     def test_from_mesh_data(self):
         file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "stanford_bunny.json")
         stanford_bunny = DisplayTriangleShell3D.load_from_file(file_path)
-        voxelization = MatrixBasedVoxelization.from_mesh_data(stanford_bunny.positions, stanford_bunny.indices, 0.005)
+        voxelization1 = MatrixBasedVoxelization.from_mesh_data(stanford_bunny.positions, stanford_bunny.indices, 0.005)
+        voxelization2 = MatrixBasedVoxelization.from_shell(stanford_bunny, 0.005)
 
-        self.assertEqual(1735, len(voxelization))
+        self.assertEqual(1735, len(voxelization1))
+        self.assertEqual(1735, len(voxelization2))
 
 
 class TestMatrixBasedVoxelizationBooleanOperation(unittest.TestCase):
