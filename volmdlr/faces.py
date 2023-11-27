7975637978a762aca6cd6b5758a4fe1b7ec27e2a
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-11-27 17:20:36 +0100

    clean

--- volmdlr/faces.py
+++ volmdlr/faces.py
@@ -190,8 +190,6 @@ class Face3D(volmdlr.core.Primitive3D):
             point = next(contour for contour in contours if isinstance(contour, volmdlr.Point3D))
             contours = [contour for contour in contours if contour is not point]
             return face.from_contours3d_and_rectangular_cut(surface, contours, point)
-        if step_id == 1477202:
-            print(True)
         return face.from_contours3d(surface, contours, name)
 
     @classmethod
