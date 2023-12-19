f5dfd4c88cc7e67e4d3cd0a83fb8c5ac524f9389
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2023-12-19 19:09:21 +0100

    debbuging step file

--- volmdlr/faces.py
+++ volmdlr/faces.py
@@ -220,7 +220,8 @@ class Face3D(volmdlr.core.Primitive3D):
             point = next(contour for contour in contours if isinstance(contour, volmdlr.Point3D))
             contours = [contour for contour in contours if contour is not point]
             return face.from_contours3d_and_rectangular_cut(surface, contours, point)
-        if step_id == 85:
+        print(step_id)
+        if step_id == 849757:
             print(True)
         return face.from_contours3d(surface, contours, step_id)
 
--- volmdlr/shells.py
+++ volmdlr/shells.py
@@ -802,6 +802,7 @@ class Shell3D(volmdlr.core.CompositePrimitive3D):
         """
         meshes = []
         for i, face in enumerate(self.faces):
+            print(face.name)
             try:
                 face_mesh = face.triangulation()
 
