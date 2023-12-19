b5ebb22f3723fd3200b460f8f06e6cc64cf72280
Pierre Gibertini <pierre.gibertini@gmail.com>
2023-12-19 16:21:33 +0100

    Update Mesh3D unit tests

--- tests/display/test_mesh3d.py
+++ tests/display/test_mesh3d.py
@@ -13,11 +13,11 @@ class TestMesh3D(unittest.TestCase):
         # Sample data for testing
         self.positions1 = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]])
         self.indices1 = np.array([[0, 1, 2]])
-        self.shell1 = Mesh3D(self.positions1, self.indices1, "Shell1")
+        self.mesh1 = Mesh3D(self.positions1, self.indices1, "Mesh1")
 
-        self.positions2 = np.array([[0, 1, 0], [1, 1, 0], [1, 0, 0]])  # Note shared vertex with shell1
+        self.positions2 = np.array([[0, 1, 0], [1, 1, 0], [1, 0, 0]])  # Note shared vertex with mesh1
         self.indices2 = np.array([[0, 1, 2]])
-        self.shell2 = Mesh3D(self.positions2, self.indices2, "Shell2")
+        self.mesh2 = Mesh3D(self.positions2, self.indices2, "Mesh2")
 
         self.positions3 = np.array(
             [
@@ -47,7 +47,7 @@ class TestMesh3D(unittest.TestCase):
                 [0, 3, 1],
             ]
         )
-        self.shell3 = Mesh3D(self.positions3, self.indices3, "Shell3")
+        self.mesh3 = Mesh3D(self.positions3, self.indices3, "Mesh3")
 
         self.positions4 = np.array(
             [
@@ -77,42 +77,53 @@ class TestMesh3D(unittest.TestCase):
                 [0, 5, 1],
             ]
         )
-        self.shell4 = Mesh3D(self.positions4, self.indices4, "Shell4")
+        self.mesh4 = Mesh3D(self.positions4, self.indices4, "Mesh4")
 
     def test_concatenate(self):
-        concatenated_shell = self.shell1.concatenate(self.shell2)
+        concatenated_mesh = self.mesh1.concatenate(self.mesh2)
 
         expected_positions = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 1, 0]])
 
-        np.testing.assert_array_equal(np.sort(concatenated_shell.positions, axis=0),
+        np.testing.assert_array_equal(np.sort(concatenated_mesh.positions, axis=0),
                                       np.sort(expected_positions, axis=0))
         # Compare indices carefully since their correctness depends on the order of positions
 
     def test_add_operator(self):
-        combined_shell = self.shell1 + self.shell2
+        combined_mesh = self.mesh1 + self.mesh2
 
         expected_positions = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0], [1, 1, 0]])
 
-        np.testing.assert_array_equal(np.sort(combined_shell.vertices, axis=0),
+        np.testing.assert_array_equal(np.sort(combined_mesh.vertices, axis=0),
                                       np.sort(expected_positions, axis=0))
         # Compare indices carefully since their correctness depends on the order of positions
 
     def test_concatenate_cube(self):
-        combined_shell = self.shell3 + self.shell4
+        combined_mesh = self.mesh3 + self.mesh4
 
-        self.assertEqual(22, len(combined_shell.triangles))
-        self.assertEqual(12, len(combined_shell.vertices))
+        self.assertEqual(22, len(combined_mesh.triangles))
+        self.assertEqual(12, len(combined_mesh.vertices))
+
+    def test_merge_cube(self):
+        merged_mesh_1 = self.mesh3.merge(self.mesh4, mutualize=False)
+
+        self.assertEqual(24, len(merged_mesh_1.triangles))
+        self.assertEqual(16, len(merged_mesh_1.vertices))
+
+        merged_mesh_2 = self.mesh3.merge(self.mesh4, mutualize=True)
+
+        self.assertEqual(22, len(merged_mesh_2.triangles))
+        self.assertEqual(12, len(merged_mesh_2.vertices))
 
     def test_equality(self):
-        self.assertNotEqual(self.shell1, self.shell2)
-        self.assertFalse(self.shell1._data_eq(self.shell2))
+        self.assertNotEqual(self.mesh1, self.mesh2)
+        self.assertFalse(self.mesh1._data_eq(self.mesh2))
 
     def test_concatenate_empty(self):
-        empty_shell = Mesh3D(np.array([]), np.array([]))
+        empty_mesh = Mesh3D(np.array([]), np.array([]))
 
-        self.assertEqual(self.shell1, self.shell1 + empty_shell)
-        self.assertEqual(self.shell1, empty_shell + self.shell1)
-        self.assertNotEqual(self.shell1, self.shell1 + self.shell2)
+        self.assertEqual(self.mesh1, self.mesh1 + empty_mesh)
+        self.assertEqual(self.mesh1, empty_mesh + self.mesh1)
+        self.assertNotEqual(self.mesh1, self.mesh1 + self.mesh2)
 
 
 if __name__ == '__main__':
