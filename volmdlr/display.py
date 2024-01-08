65dc9c15a6af5e0026aacf836584ca28b96cc295
Pierre Gibertini <pierre.gibertini@gmail.com>
2024-01-08 18:01:28 +0100

    Feat: decimate

--- volmdlr/display.py
+++ volmdlr/display.py
@@ -10,6 +10,7 @@ from typing import List, TypeVar, Union
 
 import numpy as np
 import trimesh
+import pyfqmr
 from dessia_common.core import DessiaObject, PhysicalObject
 from dessia_common.serialization import BinaryFile
 from dessia_common.typings import JsonSerializable
@@ -471,6 +472,72 @@ class Mesh3D(MeshMixin, PhysicalObject):
         """
         areas = np.sqrt((self.triangles_cross_products() ** 2).sum(axis=1)) / 2.0
         return areas.sum()
+
+    def decimate(
+        self,
+        target_count: int,
+        update_rate: int = 5,
+        aggressiveness: float = 7.0,
+        max_iterations: int = 100,
+        verbose: bool = False,
+        lossless: bool = False,
+        threshold_lossless: float = 1e-3,
+        alpha: float = 1e-9,
+        k: int = 3,
+        preserve_border: bool = True,
+    ) -> "Mesh3D":
+        """
+        Decimate the Mesh3D, and return it as a new instance.
+
+        Vertices of the mesh should be merged (and maybe rounded) for efficient decimation.
+
+        Note: threshold = alpha * pow(iteration + k, aggressiveness)
+
+        :param target_count: Target number of triangles. Not used if `lossless` is True.
+        :type target_count: int
+        :param update_rate: Number of iterations between each update. If `lossless` flag is set to True, rate is 1.
+        :type update_rate: int
+        :param aggressiveness: Parameter controlling the growth rate of the threshold at each iteration when `lossless`
+            is False.
+        :type aggressiveness: float
+        :param max_iterations: Maximal number of iterations.
+        :type max_iterations: int
+        :param verbose: Control verbosity.
+        :type verbose: bool
+        :param lossless: Use the lossless simplification method.
+        :type lossless: bool
+        :param threshold_lossless: Maximal error after which a vertex is not deleted. Only for `lossless` method.
+        :type threshold_lossless: float
+        :param alpha: Parameter for controlling the threshold growth.
+        :type alpha: float
+        :param k: Parameter for controlling the threshold growth.
+        :type k: int
+        :param preserve_border: Flag for preserving vertices on open border.
+        :type preserve_border: bool
+
+        :return: The decimated mesh.
+        :rtype: Mesh3D
+        """
+        # pylint: disable=too-many-arguments
+
+        simplifier = pyfqmr.Simplify()
+        simplifier.setMesh(self.vertices, self.triangles)
+        simplifier.simplify_mesh(
+            target_count=target_count,
+            update_rate=update_rate,
+            aggressiveness=aggressiveness,
+            max_iterations=max_iterations,
+            verbose=verbose,
+            lossless=lossless,
+            threshold_lossless=threshold_lossless,
+            alpha=alpha,
+            K=k,
+            preserve_border=preserve_border,
+        )
+
+        vertices, triangles, _ = simplifier.getMesh()
+
+        return self.__class__(vertices, triangles)
 
     @property
     def faces(self):
