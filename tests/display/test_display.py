f6cd29964bc08376e58df517ed3d8f2772db28ee
Pierre Gibertini <pierre.gibertini@gmail.com>
2024-01-10 15:22:51 +0100

    Add tests for merge_meshes=True

--- tests/display/test_display.py
+++ tests/display/test_display.py
@@ -7,6 +7,7 @@ import unittest
 
 import volmdlr.primitives3d as p3d
 from volmdlr import O2D, O3D, OXYZ, X3D, Point2D, Point3D
+from volmdlr.core import VolumeModel
 from volmdlr.curves import Circle2D
 from volmdlr.display import Mesh3D
 from volmdlr.step import Step
@@ -24,27 +25,39 @@ class TestDisplayPrimitives3D(unittest.TestCase):
         block = p3d.Block(frame=OXYZ)
         block.babylonjs()
 
+        VolumeModel([block]).babylonjs(merge_meshes=False)
+
     def test_display_extruded_profile(self):
         extruded_profile = p3d.ExtrudedProfile(
             frame=OXYZ, outer_contour2d=self.contour2d, inner_contours2d=[], extrusion_length=1.0
         )
         extruded_profile.babylonjs()
 
+        VolumeModel([extruded_profile]).babylonjs(merge_meshes=False)
+
     def test_display_revolved_profile(self):
         revolved_profile = p3d.RevolvedProfile(frame=OXYZ, contour2d=self.contour2d, axis_point=O3D, axis=X3D)
         revolved_profile.babylonjs()
+
+        VolumeModel([revolved_profile]).babylonjs(merge_meshes=False)
 
     def test_display_cylinder(self):
         cylinder = p3d.Cylinder(frame=OXYZ, radius=1.0, length=1.0)
         cylinder.babylonjs()
 
+        VolumeModel([cylinder]).babylonjs(merge_meshes=False)
+
     def test_display_hollow_cylider(self):
         hollow_cylinder = p3d.HollowCylinder(frame=OXYZ, inner_radius=0.5, outer_radius=1.0, length=1.0)
         hollow_cylinder.babylonjs()
 
+        VolumeModel([hollow_cylinder]).babylonjs(merge_meshes=False)
+
     def test_display_sphere(self):
         sphere = p3d.Sphere(O3D, 1.0)
         sphere.babylonjs()
+
+        VolumeModel([sphere]).babylonjs(merge_meshes=False)
 
     def test_display_sweep(self):
         sweep = p3d.Sweep(
@@ -54,6 +67,8 @@ class TestDisplayPrimitives3D(unittest.TestCase):
             ),
         )
         sweep.babylonjs()
+
+        VolumeModel([sweep]).babylonjs(merge_meshes=False)
 
 
 class TestDisplayStep(unittest.TestCase):
@@ -66,18 +81,22 @@ class TestDisplayStep(unittest.TestCase):
     def test_display_block(self):
         volume_model = Step.from_file(self.block_path).to_volume_model()
         volume_model.babylonjs()
+        volume_model.babylonjs(merge_meshes=False)
 
     def test_display_cheese(self):
         volume_model = Step.from_file(self.cheese_path).to_volume_model()
         volume_model.babylonjs()
+        volume_model.babylonjs(merge_meshes=False)
 
-    def test_display_bracket2_path(self):
+    def test_display_bracket2(self):
         volume_model = Step.from_file(self.bracket2_path).to_volume_model()
         volume_model.babylonjs()
+        volume_model.babylonjs(merge_meshes=False)
 
-    def test_display_engine_path(self):
+    def test_display_engine(self):
         volume_model = Step.from_file(self.engine_path).to_volume_model()
         volume_model.babylonjs()
+        volume_model.babylonjs(merge_meshes=False)
 
 
 class TestDisplaySTL(unittest.TestCase):
