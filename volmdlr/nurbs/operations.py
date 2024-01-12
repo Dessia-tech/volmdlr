1aebbcfcd863c5ad77445c16a256ee1ea83f8f06
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2024-01-12 10:36:19 +0100

    fix knotvector

--- volmdlr/nurbs/operations.py
+++ volmdlr/nurbs/operations.py
@@ -965,5 +965,5 @@ def link_curves(curves, tol: float = 1e-7, validate: bool = True):
 
     # Fix curve by appending the last knot to the end
     knotvector += [pdomain_end for _ in range(curve.degree + 1)]
-    knots, multiplicities = get_knots_and_multiplicities(knotvector)
+    knots, multiplicities = get_knots_and_multiplicities(np.asarray(knotvector, dtype=np.float64))
     return knots, multiplicities, cpts, wgts
