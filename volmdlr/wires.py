fec865a3ab82e16d71dec1264b24d1e0db4d548c
GabrielJMS <89979482+GabrielJMS@users.noreply.github.com>
2024-02-29 11:44:04 +0100

    dessia_common bug workaround: set wires to_dict user_pointers parameter to False

--- volmdlr/wires.py
+++ volmdlr/wires.py
@@ -190,7 +190,7 @@ class WireMixin:
 
     def to_dict(self, *args, **kwargs):
         """Avoids storing points in memo that makes serialization slow."""
-        return PhysicalObject.to_dict(self, use_pointers=True)
+        return PhysicalObject.to_dict(self, use_pointers=False)
 
     def length(self):
         """Returns the wire's length."""
